54c81cd506134a6eba7f7f9d565e8ada1367240b
Michael Mintz <mdmintz@gmail.com>
2015-07-23 15:33:56 -0400

    Update comments for page_utils.py

--- test_framework/fixtures/page_utils.py
+++ test_framework/fixtures/page_utils.py
@@ -5,7 +5,8 @@ This module contains useful utility methods.
 def jq_format(code):
     """
     Use before throwing raw code such as 'div[tab="advanced"]' into jQuery.
-    Similar to "json.dumps(value)".
+    Selectors with quotes inside of quotes would otherwise break jQuery. (One example)
+    This is similar to "json.dumps(value)".
     """
     code = code.replace('\\','\\\\').replace('\t','\\t').replace('\n', '\\n').replace('\"','\\\"').replace('\'','\\\'').replace('\r', '\\r').replace('\v', '\\v').replace('\a', '\\a').replace('\f', '\\f').replace('\b', '\\b').replace('\u', '\\u')
     return code
