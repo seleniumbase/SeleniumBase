c61ec805b64452ee111659acb8ee0993b31bc5a1
Michael Mintz <mdmintz@gmail.com>
2015-07-23 14:38:23 -0400

    Might be safe to use a real tab rather than 4 spaces.

--- test_framework/fixtures/page_utils.py
+++ test_framework/fixtures/page_utils.py
@@ -7,5 +7,5 @@ def jq_format(code):
     Use before throwing raw code such as 'div[tab="advanced"]' into jQuery.
     Similar to "json.dumps(value)".
     """
-    code = code.replace('\\','\\\\').replace('\t','    ').replace('\n', '\\n').replace('\"','\\\"').replace('\'','\\\'').replace('\r', '\\r').replace('\v', '\\v').replace('\a', '\\a').replace('\f', '\\f').replace('\b', '\\b').replace('\u', '\\u')
+    code = code.replace('\\','\\\\').replace('\t','\\t').replace('\n', '\\n').replace('\"','\\\"').replace('\'','\\\'').replace('\r', '\\r').replace('\v', '\\v').replace('\a', '\\a').replace('\f', '\\f').replace('\b', '\\b').replace('\u', '\\u')
     return code
