8cd0ab9f20d6a8986147988b8177407c1a3c3c76
Michael Mintz <mdmintz@gmail.com>
2015-07-23 14:22:06 -0400

    Update comments in errors.py

--- test_framework/fixtures/errors.py
+++ test_framework/fixtures/errors.py
@@ -1,12 +1,9 @@
 """
-This module contains test state related exceptions. 
-Raising one of these in a test will cause the state of the test to
-be logged appropriately.
+This module contains test-state related exceptions. 
+Raising one of these in a test will cause the
+test-state to be logged appropriately.
 """
 
-#this makes DeprecatedTest and SkipTest available, but nose currently
-#(for 3 years) has a bug that won't let us handle them properly so 
-#we define our own
 
 class BlockedTest(Exception):
     """Raise this to mark a test as Blocked"""
