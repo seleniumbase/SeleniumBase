4fff1132e5dd041c751deff5207f232d06eaaf6c
Michael Mintz <mdmintz@gmail.com>
2015-07-26 18:55:40 -0400

    Creating a local install that doesn't require MySQL

--- local_setup.py
+++ local_setup.py
@@ -1,7 +1,7 @@
 """
 The setup package to install the SeleniumSpot Test Framework plugins
-on a server machine (or a development machine that intends to write to
-a MySQL DB during test runs).
+on a development machine that DOES NOT intend to write to
+a MySQL DB during test runs.
 """
 
 from setuptools import setup, find_packages
@@ -17,7 +17,6 @@ REQUIREMENTS = [
     'chardet==2.3.0',
     'simplejson==3.7.3',
     'boto==2.38.0',
-    'MySQL-python==1.2.5',
     'pdb==0.1',
     'ipdb==0.8.1'
 ]
@@ -44,7 +43,6 @@ setup(
             'page_source = test_framework.plugins.page_source:PageSource',
             'screen_shots = test_framework.plugins.screen_shots:ScreenShots',
             'test_info = test_framework.plugins.basic_test_info:BasicTestInfo',
-            'db_reporting = test_framework.plugins.db_reporting_plugin:DBReporting',
             's3_logging = test_framework.plugins.s3_logging_plugin:S3Logging',
             'hipchat_reporting = test_framework.plugins.hipchat_reporting_plugin:HipchatReporting',
             ]
