4cfbe9d2ee56bd15e5c0f45d90b9822f6284de52
Michael Mintz <mdmintz@gmail.com>
2015-08-03 17:34:12 -0400

    Time for another version update. Now 1.1.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'seleniumbase',
-    version = '1.1.1',
+    version = '1.1.2',
     author = 'Michael Mintz',
     author_email = '@mintzworld',
     maintainer = 'Michael Mintz',
