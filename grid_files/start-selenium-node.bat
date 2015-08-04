b2ff3395328a3006f8a22a9ef219abc7e7c49f21
Michael Mintz <mdmintz@gmail.com>
2015-08-04 17:42:19 -0400

    Simplify the Selenium node-to-grid code

--- grid_files/start-selenium-node.bat
+++ grid_files/start-selenium-node.bat
@@ -1,2 +1,2 @@
 cd c:\
-java -jar selenium-server-standalone-2.47.0.jar -port 5555 -host [ENTER HOST OF THIS WORKER MACHINE ex: ec2-***-**-**-***.compute-1.amazonaws.com] -hub http://[ENTER URL OF THE GRID HUB SERVER]:4444/grid/register -browser browserName=chrome,maxInstances=5
+java -jar selenium-server-standalone-2.47.0.jar -role node -hub http://[ENTER URL OF THE GRID HUB SERVER]:4444/grid/register -browser browserName=chrome,maxInstances=5
--- grid_files/start-selenium-server.sh
+++ grid_files/start-selenium-server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-screen -S grid-server java -jar selenium-server-standalone-2.47.0.jar -role hub -host [ENTER YOUR MASTER SELENIUM SERVER HOST HERE] -port 4444
+java -jar selenium-server-standalone-2.47.0.jar -role hub
