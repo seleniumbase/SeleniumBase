57fba193bab81aa83745bdecc3ba9f2325215447
Michael Mintz <mdmintz@gmail.com>
2015-08-05 15:26:56 -0400

    All the browsers for grid nodes.

--- grid_files/start-selenium-node.bat
+++ grid_files/start-selenium-node.bat
@@ -1,2 +1,2 @@
 cd c:\
-java -jar selenium-server-standalone-2.47.1.jar -role node -hub http://[ENTER URL OF THE GRID HUB SERVER]:4444/grid/register -browser browserName=chrome,maxInstances=5
+java -jar selenium-server-standalone-2.47.1.jar -role node -hub http://[ENTER URL OF THE GRID HUB SERVER]:4444/grid/register -browser browserName=chrome,maxInstances=5 -browser browserName=firefox,maxInstances=5 -browser browserName="internet explorer",maxInstances=1
