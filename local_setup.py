323bd8326dd89d8335a9b88868d056cfae78a8cd
Michael Mintz <mdmintz@gmail.com>
2015-08-05 18:39:11 -0400

    A lot of changes means a new version update: Now 1.1.3

--- local_setup.py
+++ local_setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'seleniumbase',
-    version = '1.1.2',
+    version = '1.1.3',
     author = 'Michael Mintz',
     author_email = '@mintzworld',
     maintainer = 'Michael Mintz',
