a151732a751874182d570951f3d26205284aeb4d
Michael Mintz <mdmintz@gmail.com>
2015-08-10 13:27:18 -0400

    Up version to 1.1.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'seleniumbase',
-    version = '1.1.3',
+    version = '1.1.4',
     author = 'Michael Mintz',
     author_email = '@mintzworld',
     maintainer = 'Michael Mintz',
