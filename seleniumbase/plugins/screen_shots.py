7e99bcf1b3143d887501a0e8ef02f992acdeb65c
Michael Mintz <mdmintz@gmail.com>
2015-12-03 04:14:36 -0500

    Maybe I don't need all that code right now

--- seleniumbase/plugins/screen_shots.py
+++ seleniumbase/plugins/screen_shots.py
@@ -3,7 +3,6 @@ Contains the screenshot plugin for the selenium tests.
 """
 
 import os
-import time
 from nose.plugins import Plugin
 
 
@@ -34,15 +33,16 @@ class ScreenShots(Plugin):
             os.makedirs(test_logpath)
         screenshot_file = "%s/%s" % (test_logpath, self.logfile_name)
         test.driver.get_screenshot_as_file(screenshot_file)
-        try:
+        '''try:
             # Let humans see any errors on screen before closing the window
             test.driver.maximize_window()
+            import time
             time.sleep(0.2)  # Make sure the screen is ready
         except Exception:
             pass
         # Second screenshot at fullscreen might not be necessary
         # import base64
-        '''screen_b64 = test.driver.get_screenshot_as_base64()
+        screen_b64 = test.driver.get_screenshot_as_base64()
         screen = base64.decodestring(screen_b64)
         screenshot_file_2 = "%s/%s" % (test_logpath, self.logfile_name_2)
         f1 = open(screenshot_file_2, 'w+')
