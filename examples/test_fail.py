7796be8d12ae810a23df40a859484c4c7d7ff9b1
Michael Mintz <mdmintz@gmail.com>
2015-12-03 01:31:44 -0500

    Make the failing test example fail faster

--- examples/test_fail.py
+++ examples/test_fail.py
@@ -1,4 +1,6 @@
-""" test_fail.py """
+""" This test was made to fail on purpose to demonstrate the
+    logging capabilities of the SeleniumBase Test Framework """
+
 from seleniumbase import BaseCase
 
 
@@ -6,4 +8,4 @@ class MyTestClass(BaseCase):
 
     def test_find_army_of_robots_on_xkcd_desert_island(self):
         self.driver.get("http://xkcd.com/731/")
-        self.wait_for_element_visible("div#ARMY_OF_ROBOTS", timeout=3)
+        self.wait_for_element_visible("div#ARMY_OF_ROBOTS", timeout=0.5)
