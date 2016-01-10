2363e437fbc943d0ca619eb6462e067de7b18b27
Michael Mintz <mdmintz@gmail.com>
2016-01-10 03:00:27 -0500

    Update example test

--- examples/my_first_test.py
+++ examples/my_first_test.py
@@ -9,11 +9,10 @@ class MyTestClass(BaseCase):
         self.click('a[rel="license"]')
         text = self.wait_for_element_visible('center').text
         self.assertTrue("reuse any of my drawings" in text)
-        self.assertTrue("You can use them freely" in text)
         self.open("http://xkcd.com/1481/")
         self.click_link_text('Blag')
-        self.wait_for_text_visible("blag of the webcomic", "#site-description")
-        self.update_text_value("input#s", "Robots!\n")
+        self.wait_for_text_visible("The blag", "header h2")
+        self.update_text("input#s", "Robots!\n")
         self.wait_for_text_visible("Hooray robots!", "#content")
         self.open("http://xkcd.com/1319/")
         self.wait_for_text_visible("Automation", "div#ctitle")
