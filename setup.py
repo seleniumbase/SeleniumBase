5396e0d00b17b73846b682d64a870a1be361f818
Michael Mintz <mdmintz@gmail.com>
2016-01-16 20:52:22 -0500

    Version upgraded to 1.1.16

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages  # noqa
 
 setup(
     name='seleniumbase',
-    version='1.1.15',
+    version='1.1.16',
     url='https://github.com/mdmintz/SeleniumBase',
     author='Michael Mintz',
     author_email='@mintzworld',
