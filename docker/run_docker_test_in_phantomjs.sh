d5c282e2784a6c5fd7223b79f7072335fb973a22
Michael Mintz <mdmintz@gmail.com>
2016-01-20 13:41:40 -0500

    Drop PhantomJS test from Docker due to font issue

--- docker/run_docker_test_in_phantomjs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-# Run example test from inside Docker image
-echo "Running example SeleniumBase test from Docker with PhantomJS..."
-cd /SeleniumBase/examples/ && nosetests my_first_test.py --config=docker_config.cfg --browser=phantomjs
-exec "$@"
