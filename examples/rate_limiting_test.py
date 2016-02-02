b94e4971e3bcde718c40dc963a995e8afa5cf99f
Michael Mintz <mdmintz@gmail.com>
2016-02-02 11:56:59 -0500

    Make the rate-limiting test more fancy

--- examples/rate_limiting_test.py
+++ examples/rate_limiting_test.py
@@ -4,11 +4,11 @@ from seleniumbase.common import decorators
 
 class MyTestClass(BaseCase):
 
-    @decorators.rate_limited(4)  # The arg is max calls per second
+    @decorators.rate_limited(3.5)  # The arg is max calls per second
     def print_item(self, item):
         print item
 
     def test_rate_limited_printing(self):
         print "\nRunning rate-limited print test:"
-        for item in xrange(10):
+        for item in xrange(1,11):
             self.print_item(item)
