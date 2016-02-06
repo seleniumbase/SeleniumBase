101923dc8ee0a8841ee38b2897f8dade71373920
Michael Mintz <mdmintz@gmail.com>
2016-02-06 02:31:18 -0500

    seleniumbase version 1.1.22

--- integrations/docker/docker_setup.py
+++ integrations/docker/docker_setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages  # noqa
 
 setup(
     name='seleniumbase',
-    version='1.1.21',
+    version='1.1.22',
     author='Michael Mintz',
     author_email='@mintzworld',
     maintainer='Michael Mintz',
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages  # noqa
 
 setup(
     name='seleniumbase',
-    version='1.1.21',
+    version='1.1.22',
     url='https://github.com/mdmintz/SeleniumBase',
     author='Michael Mintz',
     author_email='@mintzworld',
