a3188f3e0014d822077f471176e92464a68b18e6
Michael Mintz <mdmintz@gmail.com>
2016-02-14 17:21:39 -0500

    Flake8 whitespace

--- examples/rate_limiting_test.py
+++ examples/rate_limiting_test.py
@@ -10,5 +10,5 @@ class MyTestClass(BaseCase):
 
     def test_rate_limited_printing(self):
         print "\nRunning rate-limited print test:"
-        for item in xrange(1,11):
+        for item in xrange(1, 11):
             self.print_item(item)
