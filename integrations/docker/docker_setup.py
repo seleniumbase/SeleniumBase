f6d50ade3abc81dd8ec0bf2cf51dad0aabc0bd75
Michael Mintz <mdmintz@gmail.com>
2016-02-22 20:47:40 -0500

    Version 1.1.33

--- integrations/docker/docker_setup.py
+++ integrations/docker/docker_setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages  # noqa
 
 setup(
     name='seleniumbase',
-    version='1.1.32',
+    version='1.1.33',
     author='Michael Mintz',
     author_email='@mintzworld',
     maintainer='Michael Mintz',
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages  # noqa
 
 setup(
     name='seleniumbase',
-    version='1.1.32',
+    version='1.1.33',
     url='http://seleniumbase.com',
     author='Michael Mintz',
     author_email='@mintzworld',
