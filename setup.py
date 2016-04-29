3fd3d809af2e0f67967db3262095f623b1dc4cb5
Michael Mintz <mdmintz@gmail.com>
2016-04-28 21:29:40 -0400

    Version 1.1.37

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages  # noqa
 
 setup(
     name='seleniumbase',
-    version='1.1.36',
+    version='1.1.37',
     url='http://seleniumbase.com',
     author='Michael Mintz',
     author_email='@mintzworld',
