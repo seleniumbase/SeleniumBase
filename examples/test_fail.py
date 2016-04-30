fc0ef6464725f179a89c0578bd7b53eabe72940d
Michael Mintz <mdmintz@gmail.com>
2016-04-29 20:41:03 -0400

    Use updated method names

--- examples/test_fail.py
+++ examples/test_fail.py
@@ -7,5 +7,5 @@ from seleniumbase import BaseCase
 class MyTestClass(BaseCase):
 
     def test_find_army_of_robots_on_xkcd_desert_island(self):
-        self.driver.get("http://xkcd.com/731/")
-        self.wait_for_element("div#ARMY_OF_ROBOTS", timeout=0.7)
+        self.open("http://xkcd.com/731/")
+        self.find_element("div#ARMY_OF_ROBOTS", timeout=0.7)
