94eecbd714e82ce179ca9985f9dd89dc72995070
Michael Mintz <mdmintz@gmail.com>
2016-05-20 23:52:53 -0400

    Add a method to extract the domain url from a full url

--- seleniumbase/fixtures/page_utils.py
+++ seleniumbase/fixtures/page_utils.py
@@ -14,3 +14,11 @@ def jq_format(code):
     code = code.replace('\v', '\\v').replace('\a', '\\a').replace('\f', '\\f')
     code = code.replace('\b', '\\b').replace('\u', '\\u').replace('\r', '\\r')
     return code
+
+
+def get_domain_url(url):
+    url_header = url.split('://')[0]
+    simple_url = url.split('://')[1]
+    base_url = simple_url.split('/')[0]
+    domain_url = url_header + '://' + base_url
+    return domain_url
