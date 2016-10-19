4c3d061d3b4233d2a74fd7d5f829985df078766d
Michael Mintz <mdmintz@gmail.com>
2016-10-19 01:59:21 -0400

    Use hard selenium version of 2.53.6

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires=[
         'pip>=8.1.2',
         'setuptools>=28.2.0',
-        'selenium>=2.53.6',
+        'selenium==2.53.6',
         'nose>=1.3.7',
         'pytest>=3.0.2',
         'six>=1.10.0',
