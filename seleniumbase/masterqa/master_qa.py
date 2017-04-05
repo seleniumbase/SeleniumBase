305737024c6d4731ff3fcc0acb6481b831acb984
Michael Mintz <mdmintz@gmail.com>
2017-04-05 00:24:27 -0400

    Small changes to the MasterQA Report layout

--- seleniumbase/masterqa/master_qa.py
+++ seleniumbase/masterqa/master_qa.py
@@ -263,13 +263,17 @@ class __MasterQATestCase__(BaseCase):
             if line[1] == '"FAILED!"' or line[1] == '"ERROR!"':
                 if not any_screenshots:
                     any_screenshots = True
-                    failure_table += '''<thead><tr><th>SCREENSHOT FILE
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        </th><th>LOCATION OF FAILURE</th></tr></thead>'''
+                    failure_table += '''<thead><tr>
+                        <th>SCREENSHOT FILE&nbsp;&nbsp;&nbsp;&nbsp;</th>
+                        <th>LOCATION OF FAILURE</th>
+                        </tr></thead>'''
+                display_url = line[3]
+                if len(display_url) > 60:
+                    display_url = display_url[0:58] + '...'
                 line = '<a href="%s">%s</a>' % (
                     "file://" + log_path + '/' + line[2], line[2]) + '''
-                    &nbsp;&nbsp;<td>
-                    ''' + '<a href="%s">%s</a>' % (line[3], line[3])
+                    &nbsp;&nbsp;&nbsp;&nbsp;<td>
+                    ''' + '<a href="%s">%s</a>' % (line[3], display_url)
                 line = line.replace('"', '')
                 failure_table += '<tr><td>%s</tr>\n' % line
         failure_table += '</tbody></table>'
