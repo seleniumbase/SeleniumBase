b76f0ee7b9a235507df44cd9a15a44e6158d0dd7
Michael Mintz <mdmintz@gmail.com>
2017-04-05 22:33:22 -0400

    Update the example tests

--- examples/my_first_test.py
+++ examples/my_first_test.py
@@ -4,18 +4,19 @@ from seleniumbase import BaseCase
 class MyTestClass(BaseCase):
 
     def test_basic(self):
-        self.open('http://xkcd.com/353/')                       # Opens the url
-        self.assert_element('img[alt="Python"]')      # Asserts element on page
-        self.click('a[rel="license"]')                 # Clicks element on page
-        xkcd_license = self.get_text('center')    # Gets text from page element
-        self.assertTrue('reuse any of my drawings' in xkcd_license)
+        self.open('http://xkcd.com/353/')            # Navigate to the web page
+        self.assert_element('img[alt="Python"]')       # Assert element on page
+        self.click('a[rel="license"]')                  # Click element on page
+        self.assert_text('copy and reuse', 'div center')  # Assert element text
         self.open('http://xkcd.com/1481/')
         image_object = self.find_element('#comic img')    # Returns the element
-        caption = image_object.get_attribute('title')  # Gets attr from element
+        caption = image_object.get_attribute('title')   # Get element attribute
         self.assertTrue('connections to the server' in caption)
-        self.click_link_text('Blag')          # Clicks link containing the text
-        self.assert_text('The blag', 'header h2')     # Asserts text in element
-        self.update_text('input#s', 'Robots!\n')  # Updates textfield with text
+        self.click_link_text('Blag')              # Click on link with the text
+        self.assert_text('xkcd', '#site-title')
+        header_text = self.get_text('header h2')  # Grab text from page element
+        self.assertTrue('The blag of the webcomic' in header_text)
+        self.update_text('input#s', 'Robots!\n')  # Fill in field with the text
         self.assert_text('Hooray robots!', '#content')
         self.open('http://xkcd.com/1319/')
         self.assert_text('Automation', 'div#ctitle')
