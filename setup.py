d8b5ee51c35afb47f8c26270c3f035c85b95f633
Michael Mintz <mdmintz@users.noreply.github.com>
2017-04-22 14:56:31 -0400

    Version 1.3.12

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages  # noqa
 
 setup(
     name='seleniumbase',
-    version='1.3.11',
+    version='1.3.12',
     description='Test Automation Framework - seleniumbase.com',
     long_description='Automation Framework for Simple & Reliable Web Testing',
     platforms='Mac * Windows * Linux * Docker',
