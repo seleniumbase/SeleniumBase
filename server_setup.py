4fef32d6d08a593e217cb61d4754b9097df23964
Michael Mintz <mdmintz@gmail.com>
2017-07-22 18:39:56 -0400

    Version 1.4.3

--- server_setup.py
+++ server_setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages  # noqa
 
 setup(
     name='seleniumbase',
-    version='1.4.2',
+    version='1.4.3',
     description='Test Automation Framework - http://seleniumbase.com',
     long_description='Automation Framework for Simple & Reliable Web Testing',
     platforms='Mac * Windows * Linux * Docker',
