c72391560a370b981aee9dd1c128473151899c07
Michael Mintz <mdmintz@gmail.com>
2018-02-08 15:21:10 -0500

    Upgrade setuptools version to 38.5.1

--- server_setup.py
+++ server_setup.py
@@ -19,7 +19,7 @@ setup(
     license='The MIT License',
     install_requires=[
         'pip>=9.0.1',
-        'setuptools>=38.2.4',
+        'setuptools>=38.5.1',
         'ipython==5.4.1',
         'selenium==2.53.6',
         'nose==1.3.7',
