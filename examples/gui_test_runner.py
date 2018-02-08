097debe3bbb6e3ca5f25128cca875196c579a8f7
Michael Mintz <mdmintz@gmail.com>
2018-02-08 15:17:31 -0500

    Update example tests.

--- examples/gui_test_runner.py
+++ examples/gui_test_runner.py
@@ -24,27 +24,27 @@ class App:
             frame, text="Basic Test Run in Chrome:", fg="blue").pack()
         self.run1 = Button(
             frame, command=self.run_1,
-            text=("pytest my_first_test.py --with-selenium"
+            text=("pytest my_first_test.py"
                   " --browser=chrome")).pack()
         self.title2 = Label(
             frame, text="Basic Test Run in Firefox:", fg="blue").pack()
         self.run2 = Button(
             frame, command=self.run_2,
             text=("pytest my_first_test.py"
-                  " --with-selenium --browser=firefox")).pack()
+                  " --browser=firefox")).pack()
         self.title3 = Label(
             frame, text="Basic Test Run in Demo Mode:", fg="blue").pack()
         self.run3 = Button(
             frame, command=self.run_3,
-            text=("nosetests my_first_test.py"
-                  " --with-selenium --browser=chrome --demo_mode")).pack()
+            text=("pytest my_first_test.py"
+                  " --browser=chrome --demo_mode")).pack()
         self.title4 = Label(
             frame,
             text="Basic Failing Test Run with Screenshots:",
             fg="blue").pack()
         self.run4 = Button(
             frame, command=self.run_4,
-            text=("nosetests test_fail.py --with-selenium --browser=chrome"
+            text=("nosetests test_fail.py --browser=chrome"
                   " --with-testing_base --demo_mode")).pack()
         self.title5 = Label(
             frame,
@@ -52,9 +52,8 @@ class App:
             fg="blue").pack()
         self.run5 = Button(
             frame, command=self.run_5,
-            text=("nosetests my_test_suite.py --with-selenium"
-                  " --browser=chrome --with-testing_base "
-                  "--report --show_report")).pack()
+            text=("nosetests my_test_suite.py --with-testing_base"
+                  " --report --show_report")).pack()
         self.title6 = Label(
             frame,
             text="Basic Failing Test Run showing the Multiple-Checks feature:",
@@ -62,54 +61,54 @@ class App:
         self.run6 = Button(
             frame, command=self.run_6,
             text=("nosetests non_terminating_checks_test.py"
-                  " --browser=chrome --with-selenium")).pack()
+                  " --browser=chrome")).pack()
         self.title7 = Label(
             frame,
-            text="MySQL DB Reporting Tests: (See ReadMe.md for Setup Steps!)",
+            text="Use MySQL DB Reporting: (See ReadMe.md for Setup Steps!)",
             fg="blue").pack()
         self.run7 = Button(
             frame, command=self.run_7,
-            text=("nosetests my_test_suite.py --with-selenium"
+            text=("nosetests my_test_suite.py"
                   " --browser=chrome --with-db_reporting")).pack()
         self.end_title = Label(frame, text="", fg="black").pack()
         self.quit = Button(frame, text="QUIT", command=frame.quit).pack()
 
     def run_1(self):
         os.system(
-            'pytest my_first_test.py --with-selenium --browser=chrome')
+            'pytest my_first_test.py --browser=chrome')
 
     def run_2(self):
         os.system(
-            'pytest my_first_test.py --with-selenium --browser=firefox')
+            'pytest my_first_test.py --browser=firefox')
 
     def run_3(self):
         os.system(
-            'nosetests my_first_test.py --with-selenium --demo_mode'
+            'pytest my_first_test.py --demo_mode'
             ' --browser=chrome')
 
     def run_4(self):
         os.system(
-            'nosetests test_fail.py --with-selenium'
+            'nosetests test_fail.py'
             ' --browser=chrome --with-testing_base --demo_mode')
 
     def run_5(self):
         os.system(
-            'nosetests my_test_suite.py --with-selenium'
-            ' --browser=chrome --with-testing_base --report --show_report')
+            'nosetests my_test_suite.py'
+            ' --with-testing_base --report --show_report')
 
     def run_6(self):
         os.system(
-            'nosetests non_terminating_checks_test.py --with-selenium'
+            'nosetests non_terminating_checks_test.py'
             ' --browser=chrome')
 
     def run_7(self):
         os.system(
-            'nosetests my_test_suite.py --with-selenium'
+            'nosetests my_test_suite.py'
             ' --browser=chrome --with-db_reporting')
 
 
 if __name__ == "__main__":
     root = Tk()
-    root.minsize(700, 444)
+    root.minsize(560, 444)
     app = App(root)
     root.mainloop()
