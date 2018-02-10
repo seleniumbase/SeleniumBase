c932f34c8ff0060d6f38598c731f996860772def
Michael Mintz <mdmintz@gmail.com>
2018-02-10 17:54:58 -0500

    Update NodeJS example test runner

--- integrations/node_js/server.js
+++ integrations/node_js/server.js
@@ -5,19 +5,19 @@ var app = express();
 var exec = require('child_process').exec;
 
 function run_my_first_test_in_firefox() {
-    exec("nosetests my_first_test.py --with-selenium --browser=firefox");
+    exec("nosetests my_first_test.py --browser=firefox");
 }
 
 function run_my_first_test_in_chrome() {
-    exec("nosetests my_first_test.py --with-selenium --browser=chrome");
+    exec("nosetests my_first_test.py --browser=chrome");
 }
 
 function run_my_first_test_in_firefox_with_demo_mode() {
-    exec("nosetests my_first_test.py --with-selenium --browser=firefox --demo_mode");
+    exec("nosetests my_first_test.py --browser=firefox --demo_mode");
 }
 
 function run_my_first_test_in_chrome_with_demo_mode() {
-    exec("nosetests my_first_test.py --with-selenium --browser=chrome --demo_mode");
+    exec("nosetests my_first_test.py --browser=chrome --demo_mode");
 }
 
 app.get('/', function(req, res) {
@@ -49,6 +49,5 @@ app.get('/run_my_first_test_in_chrome_with_demo_mode', function(req, res) {
 })
 
 app.listen(3000, "127.0.0.1", function() {
-    console.log('Server running at http://127.0.0.1:3000/');
+    console.log('Server running at http://127.0.0.1:3000/  (CTRL-C to stop)');
 });
-
