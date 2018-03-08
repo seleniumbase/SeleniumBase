db5f54b1d0984110c8d9742b01f5bb43beaecdcf
Michael Mintz <mdmintz@gmail.com>
2018-03-08 04:25:51 -0500

    Add basic_script.py

---
+++ examples/basic_script.py
@@ -0,0 +1,12 @@
+from seleniumbase import BaseCase
+
+
+class MyTestClass(BaseCase):
+
+    def test_basic(self):
+        self.open('http://xkcd.com/353/')
+        self.click('a[rel="license"]')
+        self.open('http://xkcd.com/1481/')
+        self.click("link=Blag")
+        self.update_text('input#s', 'Robots!\n')
+        self.open('http://xkcd.com/1319/')
