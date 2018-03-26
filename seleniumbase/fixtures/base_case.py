93dc550d2f30b131ca0c8fe1e07ae177797ebb51
Michael Mintz <mdmintz@gmail.com>
2018-03-25 23:39:34 -0400

    Remove unused import

--- seleniumbase/fixtures/base_case.py
+++ seleniumbase/fixtures/base_case.py
@@ -54,7 +54,6 @@ try:
 except Exception:
     # Selenium 2 (Keep compatibility with seleneium 2.53.6 if still being used)
     ENI_Exception = selenium_exceptions.ElementNotSelectableException
-from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
