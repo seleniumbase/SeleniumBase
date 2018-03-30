20f283763ec514ffbad79bcfa6b196910014c3e9
Michael Mintz <mdmintz@gmail.com>
2018-03-30 18:44:37 -0400

    Don't set a specific version for pip & setuptools

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ setup(
     maintainer='Michael Mintz',
     license='The MIT License',
     install_requires=[
-        'pip>=9.0.3',
-        'setuptools>=39.0.1',
+        'pip',
+        'setuptools',
         'ipython==5.5.0',
         'selenium==3.8.1',
         'nose==1.3.7',
