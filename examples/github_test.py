5f3e42e4b70e0145c91ff6769129a0cc9f2ffc1d
Michael Mintz <mdmintz@gmail.com>
2018-04-11 23:17:29 -0400

    Slow down the GitHub test to prevent triggering a call limit

--- examples/github_test.py
+++ examples/github_test.py
@@ -1,15 +1,24 @@
 from seleniumbase import BaseCase
+import time
 
 
 class GitHubTests(BaseCase):
 
+    # Selenium can trigger GitHub's abuse detection mechanism:
+    # "You have triggered an abuse detection mechanism."
+    # "Please wait a few minutes before you try again."
+    # To avoid this, slow down Selenium actions.
+    def slow_click(self, css_selector):
+        time.sleep(0.75)
+        self.click(css_selector)
+
     def test_github(self):
         self.open("https://github.com/")
         self.update_text("input.header-search-input", "SeleniumBase\n")
-        self.click('a[href="/seleniumbase/SeleniumBase"]')
+        self.slow_click('a[href="/seleniumbase/SeleniumBase"]')
         self.assert_element("div.repository-content")
         self.assert_text("SeleniumBase", "h1")
-        self.click('a[title="seleniumbase"]')
-        self.click('a[title="fixtures"]')
-        self.click('a[title="base_case.py"]')
+        self.slow_click('a[title="seleniumbase"]')
+        self.slow_click('a[title="fixtures"]')
+        self.slow_click('a[title="base_case.py"]')
         self.assert_text("Code", "nav a.selected")
