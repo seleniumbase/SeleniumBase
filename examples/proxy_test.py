db75cb41aa6729296192b18a7b61bf89acec7196
Michael Mintz <mdmintz@gmail.com>
2018-06-17 14:42:12 -0400

    Update proxy_test

--- examples/proxy_test.py
+++ examples/proxy_test.py
@@ -6,10 +6,9 @@ class MyTestClass(BaseCase):
 
     def test_proxy(self):
         self.open('https://ipinfo.io/')
-        ip_address = self.get_page_title().split(' ')[0]
+        ip_address = self.get_text("div.home-ip-details span.value")[1:-1]
+        self.open('https://ipinfo.io/%s' % ip_address)
         print("\n\nIP Address = %s\n" % ip_address)
-        href = '/%s' % ip_address
-        self.click('[href="%s"]' % href)
         print("Displaying Host Info:")
         print(self.get_text('table.table'))
         print("\nThe browser will close automatically in 7 seconds...")
