5531091ec52cc73196cdc0b9c8ca104ae26bbe3a
Michael Mintz <mdmintz@gmail.com>
2018-08-10 00:20:10 -0400

    Add Javascript and CSS resources to constants

--- seleniumbase/fixtures/constants.py
+++ seleniumbase/fixtures/constants.py
@@ -18,6 +18,79 @@ class Files:
     ARCHIVED_DOWNLOADS_FOLDER = "archived_files"
 
 
+class JQuery:
+    VER = "3.3.1"
+    MIN_JS = "//cdnjs.cloudflare.com/ajax/libs/jquery/%s/jquery.min.js" % VER
+
+
+class Messenger:
+    VER = "1.5.0"
+    MIN_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+               "messenger/%s/css/messenger.min.css" % VER)
+    MIN_JS = ("//cdnjs.cloudflare.com/ajax/libs/"
+              "messenger/%s/js/messenger.min.js" % VER)
+    THEME_FLAT_JS = ("//cdnjs.cloudflare.com/ajax/libs/"
+                     "messenger/%s/js/messenger-theme-flat.js" % VER)
+    THEME_FUTURE_JS = ("//cdnjs.cloudflare.com/ajax/libs/"
+                       "messenger/%s/js/messenger-theme-future.js" % VER)
+    THEME_FLAT_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+                      "messenger/%s/css/messenger-theme-flat.css" % VER)
+    THEME_FUTURE_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+                        "messenger/%s/css/messenger-theme-future.css" % VER)
+    THEME_BLOCK_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+                       "messenger/%s/css/messenger-theme-block.css" % VER)
+    THEME_AIR_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+                     "messenger/%s/css/messenger-theme-air.css" % VER)
+    THEME_ICE_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+                     "messenger/%s/css/messenger-theme-ice.css" % VER)
+    SPINNER_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+                   "messenger/%s/css/messenger-spinner.css" % VER)
+
+
+class Underscore:
+    VER = "1.9.1"
+    MIN_JS = ("//cdnjs.cloudflare.com/ajax/libs/"
+              "underscore.js/%s/underscore-min.js" % VER)
+
+
+class Backbone:
+    VER = "1.3.3"
+    MIN_JS = ("//cdnjs.cloudflare.com/ajax/libs/"
+              "backbone.js/%s/backbone-min.js" % VER)
+
+
+class BootstrapTour:
+    VER = "0.11.0"
+    MIN_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+               "bootstrap-tour/%s/css/bootstrap-tour-standalone.min.css" % VER)
+    MIN_JS = ("//cdnjs.cloudflare.com/ajax/libs/"
+              "bootstrap-tour/%s/js/bootstrap-tour-standalone.min.js" % VER)
+
+
+class Shepherd:
+    VER = "1.8.1"
+    MIN_JS = ("//cdnjs.cloudflare.com/ajax/libs/"
+              "shepherd/%s/js/shepherd.min.js" % VER)
+    THEME_ARROWS_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+                        "shepherd/%s/css/shepherd-theme-arrows.css" % VER)
+    THEME_ARR_FIX_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+                         "shepherd/%s/css/shepherd-theme-arrows-fix.css" % VER)
+    THEME_DEFAULT_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+                         "shepherd/%s/css/shepherd-theme-default.css" % VER)
+    THEME_DARK_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+                      "shepherd/%s/css/shepherd-theme-dark.css" % VER)
+    THEME_SQ_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+                    "shepherd/%s/css/shepherd-theme-square.css" % VER)
+    THEME_SQ_DK_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+                       "shepherd/%s/css/shepherd-theme-square-dark.css" % VER)
+
+
+class Tether:
+    VER = "1.4.4"
+    MIN_JS = ("https://cdnjs.cloudflare.com/ajax/libs/"
+              "tether/%s/js/tether.min.js" % VER)
+
+
 class ValidBrowsers:
     valid_browsers = (
         ["chrome", "edge", "firefox", "ie", "opera", "phantomjs", "safari"])
