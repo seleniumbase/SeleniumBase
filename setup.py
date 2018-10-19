614483330984b1a27193ca11f35f064137020927
Michael Mintz <mdmintz@gmail.com>
2018-10-19 11:17:45 -0400

    Keep "requests" and "urllib3" in sync

--- setup.py
+++ setup.py
@@ -59,7 +59,8 @@ setup(
         'chardet',
         'unittest2',
         'selenium==3.14.1',
-        'requests>=2.20.0',
+        'requests==2.20.0',  # Changing this may effect "urllib3"
+        'urllib3==1.24',  # Keep this lib in sync with "requests"
         'pytest>=3.9.1',
         'pytest-cov>=2.6.0',
         'pytest-html>=1.19.0',
