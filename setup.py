6ea85a0b29f4f5fde69ee499c2cbe7f6b926de16
Michael Mintz <mdmintz@gmail.com>
2018-11-06 00:10:25 -0500

    Version 1.17.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ except IOError:
 
 setup(
     name='seleniumbase',
-    version='1.17.2',
+    version='1.17.3',
     description='All-in-One Test Automation Framework',
     long_description=long_description,
     long_description_content_type='text/markdown',
