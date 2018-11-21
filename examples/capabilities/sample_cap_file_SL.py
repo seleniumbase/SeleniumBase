a43b81b7be84db2d1d9673fd3db5498487c5b7b7
Michael Mintz <mdmintz@gmail.com>
2018-11-21 02:54:39 -0500

    Add example desired capabilities files

---
+++ examples/capabilities/sample_cap_file_SL.py
@@ -0,0 +1,6 @@
+# Desired capabilities example file for Sauce Labs
+# Generate from https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
+caps = {}
+caps['browserName'] = "chrome"
+caps['platform'] = "macOS 10.12"
+caps['version'] = "70.0"
