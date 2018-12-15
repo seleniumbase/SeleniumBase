b2a8aa1b80f28e3e5ab3f241d90eff4c42f757ac
Michael Mintz <mdmintz@gmail.com>
2018-12-14 20:35:52 -0500

    Install chromedriver before geckodriver

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 python -m pip install --upgrade pip
 pip install -e . --upgrade --no-cache-dir --progress-bar off
-seleniumbase install geckodriver
 seleniumbase install chromedriver
+seleniumbase install geckodriver
