07140c033a2f82d55c7276f0d7ad3120b1af3208
Michael Mintz <mdmintz@gmail.com>
2019-01-14 19:59:37 -0500

    Update pytest-html to >= 1.20.0

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
         'urllib3==1.24.1',  # Keep this lib in sync with "requests"
         'pytest>=4.1.1',
         'pytest-cov>=2.6.1',
-        'pytest-html>=1.19.0',
+        'pytest-html>=1.20.0',
         'pytest-rerunfailures>=6.0',
         'pytest-xdist>=1.26.0',
         'parameterized==0.6.1',
