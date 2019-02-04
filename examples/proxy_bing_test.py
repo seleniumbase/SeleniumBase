35e2215b41dd1c213d07971382113b34852d922e
Michael Mintz <mdmintz@gmail.com>
2019-02-04 02:45:30 -0500

    Add a proxy test that gets the ip address from Bing

---
+++ examples/proxy_bing_test.py
@@ -0,0 +1,12 @@
+import time
+from seleniumbase import BaseCase
+
+
+class MyTestClass(BaseCase):
+
+    def test_proxy_on_bing(self):
+        self.open('https://www.bing.com/search?q=what+is+my+ip')
+        ip_address = self.get_text("#b_results li.b_top")
+        print("\n\n%s\n" % ip_address)
+        print("\nThe browser will close automatically in 7 seconds...")
+        time.sleep(7)
