8e2302675704e67e6e5ce4e6aeacaac306227f32
Michael Mintz <mdmintz@gmail.com>
2019-02-06 01:42:58 -0500

    Rename an example test

--- examples/get_ip_with_bing.py
+++ examples/get_ip_with_bing.py
@@ -4,7 +4,7 @@ from seleniumbase import BaseCase
 
 class MyTestClass(BaseCase):
 
-    def test_proxy_on_bing(self):
+    def test_get_ip_address_with_bing(self):
         self.open('https://www.bing.com/search?q=what+is+my+ip')
         ip_address = self.get_text("#b_results li.b_top")
         print("\n\n%s\n" % ip_address)
