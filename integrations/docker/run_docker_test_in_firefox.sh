1afa44ce47eba2e38dcdc450c706bd21f8a56bf0
Michael Mintz <mdmintz@gmail.com>
2019-02-28 04:46:30 -0500

    Fix the Docker integration

--- integrations/docker/run_docker_test_in_firefox.sh
+++ integrations/docker/run_docker_test_in_firefox.sh
@@ -2,5 +2,5 @@
 set -e
 # Run example test from inside Docker image
 echo "Running example SeleniumBase test from Docker with headless Firefox..."
-cd /SeleniumBase/examples/ && nosetests my_first_test.py --config=docker_config.cfg --browser=firefox --headless
+cd /SeleniumBase/examples/ && pytest my_first_test.py --browser=firefox --headless
 exec "$@"
