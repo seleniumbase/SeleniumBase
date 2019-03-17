573be0b50756916c051313936acb15280f049f94
Michael Mintz <mdmintz@gmail.com>
2019-03-17 02:09:30 -0400

    Add the ip_cow test

---
+++ examples/ip_cow_test.py
@@ -0,0 +1,13 @@
+import time
+from seleniumbase import BaseCase
+
+
+class MyTestClass(BaseCase):
+
+    def test_ip_cow(self):
+        self.open('https://www.ipcow.com/')
+        ip_data = self.get_text("form table")
+        print("\n\n*** IP and Browser Data: ***")
+        print(ip_data)
+        print("\nThe browser will close automatically in 7 seconds...")
+        time.sleep(7)
