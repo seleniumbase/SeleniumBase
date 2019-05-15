cc36955d98b59cd6f9b0ebe86af61fe53b5a9a6d
Michael Mintz <mdmintz@gmail.com>
2019-05-15 00:46:18 -0400

    Version 1.23.10

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ except IOError:
 
 setup(
     name='seleniumbase',
-    version='1.23.9',
+    version='1.23.10',
     description='Reliable Browser Automation & Testing Framework',
     long_description=long_description,
     long_description_content_type='text/markdown',
