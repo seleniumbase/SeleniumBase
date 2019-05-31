61ca14440f39106b6109b96919b520e40170b1f3
Michael Mintz <mdmintz@gmail.com>
2019-05-30 23:38:36 -0400

    Update a SeleniumBase tour example

--- examples/tour_examples/xkcd_tour.py
+++ examples/tour_examples/xkcd_tour.py
@@ -12,11 +12,10 @@ class MyTestClass(BaseCase):
         self.add_tour_step("Here's the daily webcomic.", "#comic img")
         self.add_tour_step("This is the title.", "#ctitle", alignment="top")
         self.add_tour_step("Click here for the next comic.", 'a[rel="next"]')
-        self.add_tour_step("Or here for the previous comic.", 'a[rel="prev"]')
+        self.add_tour_step("Click here for the previous one.", 'a[rel="prev"]')
         self.add_tour_step("Learn about the author here.", 'a[rel="author"]')
-        self.add_tour_step("Click for the license here.", 'a[rel="license"]')
-        self.add_tour_step("This selects a random comic.", 'a[href*="random"]')
+        self.add_tour_step("Click here for the license.", 'a[rel="license"]')
+        self.add_tour_step("Click for a random comic.", 'a[href*="/random/"]')
         self.add_tour_step("Thanks for taking this tour!")
-        # self.export_tour()  # Use this to export the tour as [my_tour.js]
-        self.export_tour(filename="xkcd_tour.js")  # You can customize the name
-        self.play_tour()
+        self.export_tour(filename="xkcd_tour.js")  # This exports the tour
+        self.play_tour()  # This plays the tour
