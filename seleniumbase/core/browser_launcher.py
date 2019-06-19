f0b5fead0d6a85f390052ba22bb8322ef50871fc
Michael Mintz <mdmintz@gmail.com>
2019-06-19 02:15:44 -0400

    Make headless Chrome more reliable in Docker environments

--- seleniumbase/core/browser_launcher.py
+++ seleniumbase/core/browser_launcher.py
@@ -106,6 +106,8 @@ def _set_chrome_options(
         }
     }
     chrome_options.add_experimental_option("prefs", prefs)
+    if headless:
+        chrome_options.add_experimental_option("w3c", False)
     chrome_options.add_argument("--test-type")
     chrome_options.add_argument("--log-level=3")
     chrome_options.add_argument("--no-first-run")
@@ -139,6 +141,8 @@ def _set_chrome_options(
             chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-setuid-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
     return chrome_options
 
 
