4f3dd6f8f19c3524da09c7be9aada766ca2788e3
Michael Mintz <mdmintz@gmail.com>
2019-07-17 01:36:03 -0400

    Update the example test

--- examples/my_first_test.py
+++ examples/my_first_test.py
@@ -4,16 +4,16 @@ from seleniumbase import BaseCase
 class MyTestClass(BaseCase):
 
     def test_basic(self):
-        self.open("https://xkcd.com/353/")           # Navigate browser to page
-        self.assert_element('img[alt="Python"]')       # Assert element on page
-        self.click('a[rel="license"]')                  # Click element on page
-        self.assert_text("free to copy", "div center")    # Assert text in area
+        self.open("https://xkcd.com/353/")
+        self.assert_element('img[alt="Python"]')
+        self.click('a[rel="license"]')
+        self.assert_text("free to copy and reuse")
         self.open("https://xkcd.com/1481/")
-        title = self.get_attribute("#comic img", "title")    # Get an attribute
+        title = self.get_attribute("#comic img", "title")
         self.assert_true("86,400 seconds per day" in title)
-        self.click("link=Blag")                                 # Click on link
+        self.click("link=Blag")
         self.assert_text("The blag of the webcomic", "h2")
-        self.update_text("input#s", "Robots!\n")                    # Type text
+        self.update_text("input#s", "Robots!\n")
         self.assert_text("Hooray robots!", "#content")
         self.open("https://xkcd.com/1319/")
         self.assert_exact_text("Automation", "#ctitle")
