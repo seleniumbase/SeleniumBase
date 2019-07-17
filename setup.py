764e2ff31874554d5c92ea1e0334664ca8e8a0e3
Michael Mintz <mdmintz@gmail.com>
2019-07-17 01:32:09 -0400

    Use pytest-html==1.20.0 (later versions have errors)

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
         'pytest>=4.6.4',  # Keep at >=4.6.4 for Python 2 compatibility
         'pytest-cov>=2.7.1',
         'pytest-forked>=1.0.2',
-        'pytest-html>=1.21.1',
+        'pytest-html==1.20.0',  # Keep at 1.20.0 (later versions have errors)
         'pytest-metadata>=1.8.0',
         'pytest-ordering>=0.6',
         'pytest-rerunfailures>=7.0',
