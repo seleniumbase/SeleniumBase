ef2918a49c1b56d488f17de50445fb9e95f98550
Michael Mintz <mdmintz@gmail.com>
2019-07-30 02:56:35 -0400

    Use SeleniumBase's virtual_display package

--- seleniumbase/fixtures/base_case.py
+++ seleniumbase/fixtures/base_case.py
@@ -3256,7 +3256,8 @@ class BaseCase(unittest.TestCase):
                 self.case_start_time = int(time.time() * 1000)
             if self.headless:
                 try:
-                    from pyvirtualdisplay import Display
+                    # from pyvirtualdisplay import Display  # Skip for own lib
+                    from seleniumbase.virtual_display.display import Display
                     self.display = Display(visible=0, size=(1440, 1880))
                     self.display.start()
                     self.headless_active = True
