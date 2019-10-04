9df29ddaedee9e82b72eacaf7cd32d74c6d45cb9
Michael Mintz <mdmintz@gmail.com>
2019-10-04 01:14:37 -0400

    Add Python 3.8 to the list of supported Python versions

--- setup.py
+++ setup.py
@@ -78,6 +78,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     install_requires=[
         'pip',
