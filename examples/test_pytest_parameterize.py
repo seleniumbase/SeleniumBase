6e9627eabf0851159521cfc3d6caa91188425de4
Michael Mintz <mdmintz@gmail.com>
2019-10-20 14:13:28 -0400

    Add test_pytest_parameterize.py

---
+++ examples/test_pytest_parameterize.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.mark.parametrize('value', ["pytest", "selenium"])
+def test_sb_fixture_with_no_class(sb, value):
+    sb.open("https://google.com/ncr")
+    sb.update_text('input[title="Search"]', value + '\n')
+    sb.assert_text(value, "div#center_col")
+
+
+class Test_SB_Fixture():
+    @pytest.mark.parametrize('value', ["pytest", "selenium"])
+    def test_sb_fixture_inside_class(self, sb, value):
+        sb.open("https://google.com/ncr")
+        sb.update_text('input[title="Search"]', value + '\n')
+        sb.assert_text(value, "div#center_col")
