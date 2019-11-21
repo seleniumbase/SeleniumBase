acd0b6b46554cfa40b19dd32db958ae275324274
Michael Mintz <mdmintz@gmail.com>
2019-11-21 00:19:57 -0500

    Update a test comment

--- examples/test_inspect_html.py
+++ examples/test_inspect_html.py
@@ -1,6 +1,7 @@
 """
 Uses the SeleniumBase implementation of HTML-Inspector to inspect the HTML.
 See https://github.com/philipwalton/html-inspector for more details.
+(Only works on Chrome and Chromium-based browsers.)
 """
 
 from seleniumbase import BaseCase
