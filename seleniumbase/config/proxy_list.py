3c8665bc6861c12c0885820bdfa5bfbccd864a6f
Michael Mintz <mdmintz@gmail.com>
2019-12-26 15:29:10 -0500

    Update example proxy list

--- seleniumbase/config/proxy_list.py
+++ seleniumbase/config/proxy_list.py
@@ -20,8 +20,7 @@ you can try finding one from one of following sites:
 
 PROXY_LIST = {
     "example1": "104.236.248.219:3128",  # (Example) - set your own proxy here
-    "example2": "81.4.255.25:3128",  # (Example) - set your own proxy here
-    "example3": "52.187.121.7:3128",  # (Example) - set your own proxy here
+    "example2": "52.187.121.7:3128",  # (Example) - set your own proxy here
     "proxy1": None,
     "proxy2": None,
     "proxy3": None,
