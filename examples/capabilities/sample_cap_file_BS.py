f48580058a7811d1157da473cb2ce69251831f98
Michael Mintz <mdmintz@gmail.com>
2019-12-31 17:37:27 -0500

    Update example desired capabilities

--- examples/capabilities/sample_cap_file_BS.py
+++ examples/capabilities/sample_cap_file_BS.py
@@ -2,10 +2,9 @@
 # Generate from https://www.browserstack.com/automate/capabilities
 desired_cap = {
     'os': 'OS X',
-    'os_version': 'Sierra',
+    'os_version': 'High Sierra',
     'browser': 'Chrome',
-    'browser_version': '70.0',
+    'browser_version': '77.0',
     'browserstack.local': 'false',
-    'browserstack.selenium_version': '3.14.0',
-    'browserstack.chrome.driver': '2.43'
+    'browserstack.selenium_version': '3.141.59'
 }
--- examples/capabilities/sample_cap_file_SL.py
+++ examples/capabilities/sample_cap_file_SL.py
@@ -1,6 +1,9 @@
 # Desired capabilities example file for Sauce Labs
 # Generate from https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
-caps = {}
-caps['browserName'] = "chrome"
-caps['platform'] = "macOS 10.12"
-caps['version'] = "70.0"
+capabilities = {
+    'browserName': 'firefox',
+    'browserVersion': '70.0',
+    'platformName': 'macOS 10.13',
+    'sauce:options': {
+    }
+}
