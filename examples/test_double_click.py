a49a3c133478c01774adfe8853b608e110a5a2e6
Michael Mintz <mdmintz@gmail.com>
2020-01-04 18:55:30 -0500

    Update a test for entering iframes and double-clicking

--- examples/test_double_click.py
+++ examples/test_double_click.py
@@ -3,9 +3,18 @@ from seleniumbase import BaseCase
 
 class MyTestClass(BaseCase):
 
-    def test_double_click(self):
+    def test_double_click_and_switch_to_frame(self):
         self.open("https://www.w3schools.com/jsref"
                   "/tryit.asp?filename=tryjsref_ondblclick")
-        self.switch_to_frame("iframeResult")
+        self.ad_block()
+        self.switch_to_frame("#iframeResult")
+        self.double_click('[ondblclick="myFunction()"]')
+        self.assert_text("Hello World", "#demo")
+
+    def test_double_click_and_switch_to_frame_of_element(self):
+        self.open("https://www.w3schools.com/jsref"
+                  "/tryit.asp?filename=tryjsref_ondblclick")
+        self.ad_block()
+        self.switch_to_frame_of_element('[ondblclick="myFunction()"]')
         self.double_click('[ondblclick="myFunction()"]')
         self.assert_text("Hello World", "#demo")
