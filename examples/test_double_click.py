7e358d7d32233ffce863307b745d92f017be5a69
Michael Mintz <mdmintz@gmail.com>
2020-01-07 00:35:42 -0500

    Update an example test

--- examples/test_double_click.py
+++ examples/test_double_click.py
@@ -1,17 +1,17 @@
 from seleniumbase import BaseCase
 
 
-class MyTestClass(BaseCase):
+class DoubleClickTestClass(BaseCase):
 
-    def test_double_click_and_switch_to_frame(self):
+    def test_switch_to_frame_and_double_click(self):
         self.open("https://www.w3schools.com/jsref"
                   "/tryit.asp?filename=tryjsref_ondblclick")
         self.ad_block()
-        self.switch_to_frame("#iframeResult")
+        self.switch_to_frame("iframe#iframeResult")
         self.double_click('[ondblclick="myFunction()"]')
         self.assert_text("Hello World", "#demo")
 
-    def test_double_click_and_switch_to_frame_of_element(self):
+    def test_switch_to_frame_of_element_and_double_click(self):
         self.open("https://www.w3schools.com/jsref"
                   "/tryit.asp?filename=tryjsref_ondblclick")
         self.ad_block()
