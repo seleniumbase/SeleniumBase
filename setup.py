d85e28862686243cd7c41fc3028fbbe290e06c11
Michael Mintz <mdmintz@gmail.com>
2020-01-17 00:20:14 -0500

    Version 1.34.21

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ if sys.argv[-1] == 'publish':
 
 setup(
     name='seleniumbase',
-    version='1.34.20',
+    version='1.34.21',
     description='Fast, Easy, and Reliable Browser Automation & Testing.',
     long_description=long_description,
     long_description_content_type='text/markdown',
