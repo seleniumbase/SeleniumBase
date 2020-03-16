e891cd3ede58ce275291ae5c3c76fdc65b2a6f77
Michael Mintz <mdmintz@gmail.com>
2020-03-16 01:00:56 -0400

    Add comments to avoid flake8 issues

--- examples/translations/chinese_test_1.py
+++ examples/translations/chinese_test_1.py
@@ -1,7 +1,7 @@
-from seleniumbase.translate.chinese import 硒测试用例
+from seleniumbase.translate.chinese import 硒测试用例  # noqa
 
 
-class 测试类(硒测试用例):
+class 测试类(硒测试用例):  # noqa
 
     def test_例子1(self):
         self.开启网址("https://xkcd.in/comic?lg=cn&id=353")
