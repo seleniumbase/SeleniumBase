c36c8ff7fcf650f9b6265f89f1f8859fca544342
Michael Mintz <mdmintz@gmail.com>
2020-03-18 15:42:14 -0400

    Update a test selector

--- examples/parameterized_test.py
+++ examples/parameterized_test.py
@@ -12,5 +12,5 @@ class GoogleTestClass(BaseCase):
     def test_parameterized_google_search(self, search_term, expected_text):
         self.open('https://google.com/ncr')
         self.update_text('input[title="Search"]', search_term + '\n')
-        self.assert_element('#resultStats')
+        self.assert_element('#result-stats')
         self.assert_text(expected_text, '#search')
