4fdb7179d99eadda5dfdb45f7e4fb0afc3b166a8
Michael Mintz <mdmintz@gmail.com>
2020-03-20 00:31:10 -0400

    Update the Chinese translation of SeleniumBase

--- seleniumbase/translate/chinese.py
+++ seleniumbase/translate/chinese.py
@@ -16,6 +16,10 @@ class 硒测试用例(BaseCase):  # noqa
         # double_click(selector)
         self.double_click(*args, **kwargs)
 
+    def 慢单击(self, *args, **kwargs):
+        # slow_click(selector)
+        self.slow_click(*args, **kwargs)
+
     def 单击链接文本(self, *args, **kwargs):
         # click_link_text(link_text)
         self.click_link_text(*args, **kwargs)
@@ -48,6 +52,22 @@ class 硒测试用例(BaseCase):  # noqa
         # assert_title(title)
         self.assert_title(*args, **kwargs)
 
+    def 断言为真(self, *args, **kwargs):
+        # assert_true(expr)
+        self.assert_true(*args, **kwargs)
+
+    def 断言为假(self, *args, **kwargs):
+        # assert_false(expr)
+        self.assert_false(*args, **kwargs)
+
+    def 断言等于(self, *args, **kwargs):
+        # assert_equal(first, second)
+        self.assert_equal(*args, **kwargs)
+
+    def 断言不等于(self, *args, **kwargs):
+        # assert_not_equal(first, second)
+        self.assert_not_equal(*args, **kwargs)
+
     def 刷新页面(self, *args, **kwargs):
         # refresh_page()
         self.refresh_page(*args, **kwargs)
@@ -68,9 +88,9 @@ class 硒测试用例(BaseCase):  # noqa
         # go_forward()
         self.go_forward(*args, **kwargs)
 
-    def 文本是否存在(self, *args, **kwargs):
-        # is_text_visible(selector)
-        self.is_element_visible(*args, **kwargs)
+    def 文本是否显示(self, *args, **kwargs):
+        # is_text_visible(text, selector="html")
+        self.is_text_visible(*args, **kwargs)
 
     def 元素是否可见(self, *args, **kwargs):
         # is_element_visible(selector)
@@ -81,24 +101,36 @@ class 硒测试用例(BaseCase):  # noqa
         self.is_element_present(*args, **kwargs)
 
     def 等待文本(self, *args, **kwargs):
-        # wait_for_text(text, selector)
+        # wait_for_text(text, selector="html")
         self.wait_for_text(*args, **kwargs)
 
     def 等待元素(self, *args, **kwargs):
         # wait_for_element(selector)
         self.wait_for_element(*args, **kwargs)
 
+    def 睡(self, *args, **kwargs):
+        # sleep(seconds)
+        self.sleep(*args, **kwargs)
+
+    def 提交(self, *args, **kwargs):
+        # submit(selector)
+        self.submit(*args, **kwargs)
+
     def JS单击(self, *args, **kwargs):
         # js_click(selector)
         self.js_click(*args, **kwargs)
 
-    def 检查JS错误(self, *args, **kwargs):
-        # assert_no_js_errors()
-        self.assert_no_js_errors(*args, **kwargs)
-
     def 检查HTML(self, *args, **kwargs):
         # inspect_html()
         self.inspect_html(*args, **kwargs)
+
+    def 保存截图(self, *args, **kwargs):
+        # save_screenshot(name)
+        self.save_screenshot(*args, **kwargs)
+
+    def 选择文件(self, *args, **kwargs):
+        # choose_file(selector, file_path)
+        self.choose_file(*args, **kwargs)
 
     def 执行脚本(self, *args, **kwargs):
         # execute_script(script)
@@ -108,11 +140,19 @@ class 硒测试用例(BaseCase):  # noqa
         # ad_block()
         self.ad_block(*args, **kwargs)
 
+    def 跳过(self, *args, **kwargs):
+        # skip(reason="")
+        self.skip(*args, **kwargs)
+
     def 检查断开的链接(self, *args, **kwargs):
         # assert_no_404_errors()
         self.assert_no_404_errors(*args, **kwargs)
 
-    def 切换到相框(self, *args, **kwargs):
+    def 检查JS错误(self, *args, **kwargs):
+        # assert_no_js_errors()
+        self.assert_no_js_errors(*args, **kwargs)
+
+    def 切换到帧(self, *args, **kwargs):
         # switch_to_frame(frame)
         self.switch_to_frame(*args, **kwargs)
 
@@ -139,6 +179,10 @@ class 硒测试用例(BaseCase):  # noqa
     def 亮点单击(self, *args, **kwargs):
         # highlight_click(selector)
         self.highlight_click(*args, **kwargs)
+
+    def 滚动到(self, *args, **kwargs):
+        # scroll_to(selector)
+        self.scroll_to(*args, **kwargs)
 
     def 滚动到顶部(self, *args, **kwargs):
         # scroll_to_top()
