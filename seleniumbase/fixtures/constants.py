01cd41c0ec97be2882fb5364ba0969fd4b3e66b6
Michael Mintz <mdmintz@gmail.com>
2020-04-09 02:38:11 -0400

    Use complete cloudflare links for accessing libraries

--- seleniumbase/fixtures/constants.py
+++ seleniumbase/fixtures/constants.py
@@ -33,107 +33,110 @@ class VisualBaseline:
 
 class JQuery:
     VER = "3.4.1"
-    MIN_JS = "//cdnjs.cloudflare.com/ajax/libs/jquery/%s/jquery.min.js" % VER
-    # MIN_JS = "//ajax.aspnetcdn.com/ajax/jQuery/jquery-%s.min.js" % VER
-    # MIN_JS = "//ajax.googleapis.com/ajax/libs/jquery/%s/jquery.min.js" % VER
+    MIN_JS = (
+        "https://cdnjs.cloudflare.com/ajax/libs/jquery/%s/jquery.min.js" % VER)
+    # MIN_JS = (
+    #    "https://ajax.aspnetcdn.com/ajax/jQuery/jquery-%s.min.js" % VER)
+    # MIN_JS = (
+    #    "https://ajax.googleapis.com/ajax/libs/jquery/%s/jquery.min.js" % VER)
 
 
 class Messenger:
     VER = "1.5.0"
-    MIN_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    MIN_CSS = ("https://cdnjs.cloudflare.com/ajax/libs/"
                "messenger/%s/css/messenger.min.css" % VER)
-    MIN_JS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    MIN_JS = ("https://cdnjs.cloudflare.com/ajax/libs/"
               "messenger/%s/js/messenger.min.js" % VER)
-    THEME_FLAT_JS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    THEME_FLAT_JS = ("https://cdnjs.cloudflare.com/ajax/libs/"
                      "messenger/%s/js/messenger-theme-flat.js" % VER)
-    THEME_FUTURE_JS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    THEME_FUTURE_JS = ("https://cdnjs.cloudflare.com/ajax/libs/"
                        "messenger/%s/js/messenger-theme-future.js" % VER)
-    THEME_FLAT_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    THEME_FLAT_CSS = ("https://cdnjs.cloudflare.com/ajax/libs/"
                       "messenger/%s/css/messenger-theme-flat.min.css" % VER)
-    THEME_FUTURE_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    THEME_FUTURE_CSS = ("https://cdnjs.cloudflare.com/ajax/libs/"
                         "messenger/%s/css/"
                         "messenger-theme-future.min.css" % VER)
-    THEME_BLOCK_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    THEME_BLOCK_CSS = ("https://cdnjs.cloudflare.com/ajax/libs/"
                        "messenger/%s/css/messenger-theme-block.min.css" % VER)
-    THEME_AIR_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    THEME_AIR_CSS = ("https://cdnjs.cloudflare.com/ajax/libs/"
                      "messenger/%s/css/messenger-theme-air.min.css" % VER)
-    THEME_ICE_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    THEME_ICE_CSS = ("https://cdnjs.cloudflare.com/ajax/libs/"
                      "messenger/%s/css/messenger-theme-ice.min.css" % VER)
-    SPINNER_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    SPINNER_CSS = ("https://cdnjs.cloudflare.com/ajax/libs/"
                    "messenger/%s/css/messenger-spinner.min.css" % VER)
 
 
 class Underscore:
     VER = "1.9.1"
-    MIN_JS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    MIN_JS = ("https://cdnjs.cloudflare.com/ajax/libs/"
               "underscore.js/%s/underscore-min.js" % VER)
 
 
 class Backbone:
     VER = "1.4.0"
-    MIN_JS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    MIN_JS = ("https://cdnjs.cloudflare.com/ajax/libs/"
               "backbone.js/%s/backbone-min.js" % VER)
 
 
 class HtmlInspector:
     VER = "0.8.2"
-    MIN_JS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    MIN_JS = ("https://cdnjs.cloudflare.com/ajax/libs/"
               "html-inspector/%s/html-inspector.min.js" % VER)
 
 
 class BootstrapTour:
     VER = "0.11.0"
-    MIN_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    MIN_CSS = ("https://cdnjs.cloudflare.com/ajax/libs/"
                "bootstrap-tour/%s/css/bootstrap-tour-standalone.min.css" % VER)
-    MIN_JS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    MIN_JS = ("https://cdnjs.cloudflare.com/ajax/libs/"
               "bootstrap-tour/%s/js/bootstrap-tour-standalone.min.js" % VER)
 
 
 class Hopscotch:
     VER = "0.3.1"
-    MIN_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    MIN_CSS = ("https://cdnjs.cloudflare.com/ajax/libs/"
                "hopscotch/%s/css/hopscotch.min.css" % VER)
-    MIN_JS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    MIN_JS = ("https://cdnjs.cloudflare.com/ajax/libs/"
               "hopscotch/%s/js/hopscotch.min.js" % VER)
 
 
 class IntroJS:
     VER = "2.9.3"
-    MIN_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    MIN_CSS = ("https://cdnjs.cloudflare.com/ajax/libs/"
                "intro.js/%s/introjs.css" % VER)
-    MIN_JS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    MIN_JS = ("https://cdnjs.cloudflare.com/ajax/libs/"
               "intro.js/%s/intro.min.js" % VER)
 
 
 class JqueryConfirm:
     VER = "3.3.4"
-    MIN_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    MIN_CSS = ("https://cdnjs.cloudflare.com/ajax/libs/"
                "jquery-confirm/%s/jquery-confirm.min.css" % VER)
-    MIN_JS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    MIN_JS = ("https://cdnjs.cloudflare.com/ajax/libs/"
               "jquery-confirm/%s/jquery-confirm.min.js" % VER)
 
 
 class Shepherd:
     VER = "1.8.1"
-    MIN_JS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    MIN_JS = ("https://cdnjs.cloudflare.com/ajax/libs/"
               "shepherd/%s/js/shepherd.min.js" % VER)
-    THEME_ARROWS_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    THEME_ARROWS_CSS = ("https://cdnjs.cloudflare.com/ajax/libs/"
                         "shepherd/%s/css/shepherd-theme-arrows.css" % VER)
-    THEME_ARR_FIX_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    THEME_ARR_FIX_CSS = ("https://cdnjs.cloudflare.com/ajax/libs/"
                          "shepherd/%s/css/shepherd-theme-arrows-fix.css" % VER)
-    THEME_DEFAULT_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    THEME_DEFAULT_CSS = ("https://cdnjs.cloudflare.com/ajax/libs/"
                          "shepherd/%s/css/shepherd-theme-default.css" % VER)
-    THEME_DARK_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    THEME_DARK_CSS = ("https://cdnjs.cloudflare.com/ajax/libs/"
                       "shepherd/%s/css/shepherd-theme-dark.css" % VER)
-    THEME_SQ_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    THEME_SQ_CSS = ("https://cdnjs.cloudflare.com/ajax/libs/"
                     "shepherd/%s/css/shepherd-theme-square.css" % VER)
-    THEME_SQ_DK_CSS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    THEME_SQ_DK_CSS = ("https://cdnjs.cloudflare.com/ajax/libs/"
                        "shepherd/%s/css/shepherd-theme-square-dark.css" % VER)
 
 
 class Tether:
     VER = "1.4.7"
-    MIN_JS = ("//cdnjs.cloudflare.com/ajax/libs/"
+    MIN_JS = ("https://cdnjs.cloudflare.com/ajax/libs/"
               "tether/%s/js/tether.min.js" % VER)
 
 
