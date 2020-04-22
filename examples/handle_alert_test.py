abf9816f708f730f1ce5e65708bff690e69c50de
Michael Mintz <mdmintz@gmail.com>
2020-04-22 12:45:11 -0400

    Update the alert test

--- examples/handle_alert_test.py
+++ examples/handle_alert_test.py
@@ -1,4 +1,3 @@
-import time
 from seleniumbase import BaseCase
 
 
@@ -7,6 +6,12 @@ class MyTestClass(BaseCase):
     def test_alerts(self):
         self.open("about:blank")
         self.execute_script('window.alert("ALERT!!!")')
-        time.sleep(1.2)  # Not needed (Lets you see the alert pop up)
+        self.sleep(1.2)  # Not needed (Lets you see the alert pop up)
         self.wait_for_and_accept_alert()
-        time.sleep(0.8)  # Not needed (Lets you see the alert go away)
+        self.sleep(0.8)  # Not needed (Lets you see the alert go away)
+        self.execute_script('window.prompt("My Prompt","defaultText");')
+        self.sleep(1.2)  # Not needed (Lets you see the alert pop up)
+        alert = self.wait_for_and_switch_to_alert(timeout=2)
+        self.assert_equal(alert.text, "My Prompt")  # Not the input field
+        self.wait_for_and_dismiss_alert()
+        self.sleep(0.8)  # Not needed (Lets you see the alert go away)
