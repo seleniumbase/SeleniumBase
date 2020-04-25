57fb42923d00193fa35a65fcfd381647d5a887fa
Michael Mintz <mdmintz@gmail.com>
2020-04-25 16:50:00 -0400

    Update an example

--- examples/boilerplates/file_parsing/parse_files.py
+++ examples/boilerplates/file_parsing/parse_files.py
@@ -15,27 +15,27 @@ class ParseTestCase(BaseCase):
         # Example of parsing data from a file
         with open('qa_login_example.txt') as f:
             file_lines = [line.rstrip() for line in f]
-            for line in file_lines:
-                line_items = line.split(',')
-                if line_items[0] == user_type:
-                    return line_items[1], line_items[2]
+        for line in file_lines:
+            line_items = line.split(',')
+            if line_items[0] == user_type:
+                return line_items[1], line_items[2]
 
     def get_all_login_credentials(self):
         # Example of parsing data from a file (Method 2)
         keys = {}
         with open('staging_login_example.txt') as f:
             file_lines = [line.rstrip() for line in f]
-            for line in file_lines:
-                line_items = line.split(',')
-                if line_items[0] == 'admin':
-                    keys['admin'] = (
-                        {'username': line_items[1], 'password': line_items[2]})
-                if line_items[0] == 'employee':
-                    keys['employee'] = (
-                        {'username': line_items[1], 'password': line_items[2]})
-                if line_items[0] == 'customer':
-                    keys['customer'] = (
-                        {'username': line_items[1], 'password': line_items[2]})
+        for line in file_lines:
+            line_items = line.split(',')
+            if line_items[0] == 'admin':
+                keys['admin'] = (
+                    {'username': line_items[1], 'password': line_items[2]})
+            if line_items[0] == 'employee':
+                keys['employee'] = (
+                    {'username': line_items[1], 'password': line_items[2]})
+            if line_items[0] == 'customer':
+                keys['customer'] = (
+                    {'username': line_items[1], 'password': line_items[2]})
         return keys
 
 
