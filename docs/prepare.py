fa6503efbc0cb8cf2e34e8571f2a9566903d4b23
Andrzej Klajnert <github@aklajnert.pl>
2020-05-11 19:45:23 +0200

    Fixed flake8

--- docs/prepare.py
+++ docs/prepare.py
@@ -4,7 +4,9 @@ from pathlib import Path
 
 GITHUB_URL = r"https://github.com/seleniumbase/SeleniumBase/blob/master/"
 ROOT_DIR = Path(__file__).parents[1]
-URL_PATTERN = re.compile(r"(?:\(|<a href=\")(?P<url>{}[\w/.]+\.md)(?:\)|\")".format(GITHUB_URL))
+URL_PATTERN = re.compile(
+    r"(?:\(|<a href=\")(?P<url>{}[\w/.]+\.md)(?:\)|\")".format(GITHUB_URL)
+)
 MD_PATH_PATTERN = re.compile(r"\[.*\]\((?P<path>[\w\\._/]+\.md)\)")
 HEADER_PATTERN = re.compile(
     r"^(?P<level>#+)\s*(<[\w\s=\":/.]+>)?\s*\**(?P<header>.*[\w`]):?\**\s*$",
@@ -13,9 +15,11 @@ HEADER_PATTERN = re.compile(
 
 PROCESSED_PATHS = set()
 
+
 def normalize_path(path):
     path = Path(path).absolute().relative_to(ROOT_DIR)
     return str(path).replace("\\", "/")
+
 
 def read_file(file_name):
     path = ROOT_DIR / file_name
@@ -28,7 +32,7 @@ def process_file(file_name):
     content = read_file(file_name)
     urls = URL_PATTERN.findall(content)
     content = content.replace("<br />", "  \n")
-    content = re.sub(HEADER_PATTERN, "\g<level> \g<header>", content)
+    content = re.sub(HEADER_PATTERN, r"\g<level> \g<header>", content)
     directory = "/".join(normalize_path(file_name).split("/")[:-1])
 
     paths = set()
@@ -37,9 +41,9 @@ def process_file(file_name):
     for md_path in md_paths:
         path = md_path.lstrip("/")
         if (ROOT_DIR / directory / path).exists():
-            path = (ROOT_DIR / directory / path)
+            path = ROOT_DIR / directory / path
         else:
-            path = (ROOT_DIR / path)
+            path = ROOT_DIR / path
         path = path.resolve().relative_to(ROOT_DIR)
         paths.add(normalize_path(path))
         content = content.replace("(" + md_path + ")", normalize_path(path))
@@ -47,8 +51,9 @@ def process_file(file_name):
     for url in urls:
         path = url[len(GITHUB_URL):]
         paths.add(path)
-        content = content.replace(url, normalize_path(os.path.relpath(path, directory)))
-
+        content = content.replace(
+            url, normalize_path(os.path.relpath(path, directory))
+        )
 
     output_path = ROOT_DIR / "docs" / file_name
     if not output_path.parent.is_dir():
