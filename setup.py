e919d64fb0194d8d6676a6f2c469e6f5dcdecfd9
Michael Mintz <mdmintz@gmail.com>
2020-05-29 18:27:56 -0400

    Version 1.39.2

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ if sys.argv[-1] == 'publish':
 
 setup(
     name='seleniumbase',
-    version='1.39.1',
+    version='1.39.2',
     description='Fast, Easy, and Reliable Browser Automation & Testing.',
     long_description=long_description,
     long_description_content_type='text/markdown',
