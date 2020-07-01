a00720b84d2588c91045ef661fb8b26670f59fa8
Michael Mintz <mdmintz@gmail.com>
2020-07-01 14:14:22 -0400

    Update an error message

--- seleniumbase/console_scripts/sb_mkdir.py
+++ seleniumbase/console_scripts/sb_mkdir.py
@@ -58,8 +58,7 @@ def main():
             'Directory name must not include slashes ("/", "\\")!')
     elif os.path.exists(os.getcwd() + '/' + dir_name):
         error_msg = (
-            'Directory "%s" already exists in the current path!\n'
-            '' % dir_name)
+            'Directory "%s" already exists in the current path!' % dir_name)
     if error_msg:
         error_msg = c5 + error_msg + cr
         invalid_run_command(error_msg)
