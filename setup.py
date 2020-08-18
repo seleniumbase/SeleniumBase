229702f6813be4896ac4ff9f2ebd8eda89a40332
Michael Mintz <mdmintz@gmail.com>
2020-08-18 12:41:13 -0400

    Version 1.46.10

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ if sys.argv[-1] == 'publish':
 
 setup(
     name='seleniumbase',
-    version='1.46.9',
+    version='1.46.10',
     description='Web Automation and Test Framework - https://seleniumbase.io',
     long_description=long_description,
     long_description_content_type='text/markdown',
