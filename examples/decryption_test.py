bd8deac4de77dfd0220bddd4e072c236bf6b87ce
Michael Mintz <mdmintz@gmail.com>
2020-09-18 16:51:53 -0400

    Update example tests

--- examples/decryption_test.py
+++ examples/decryption_test.py
@@ -11,13 +11,13 @@ class MyTestClass(BaseCase):
 
     def test_rate_limited_printing(self):
         self.open("https://www.saucedemo.com/")
-        self.update_text("#user-name", "standard_user")
+        self.type("#user-name", "standard_user")
 
         encrypted_password = "$^*ENCRYPT=S3BDTAdCWzMmKEY8Gjg=?&#$"
         print("\nEncrypted Password = %s" % encrypted_password)
         password = encryption.decrypt(encrypted_password)
         print("Decrypted Password = %s" % password)
-        self.update_text("#password", password)
+        self.type("#password", password)
 
         self.click('input[type="submit"]')
         self.assert_text("Products", "div.product_label")
--- examples/test_usefixtures.py
+++ examples/test_usefixtures.py
@@ -6,7 +6,7 @@ class Test_UseFixtures():
     def test_usefixtures_on_class(self):
         sb = self.sb
         sb.open("https://google.com/ncr")
-        sb.update_text('input[title="Search"]', 'SeleniumBase\n')
+        sb.type('input[title="Search"]', 'SeleniumBase\n')
         sb.click('a[href*="github.com/seleniumbase/SeleniumBase"]')
         sb.assert_text("SeleniumBase", 'strong[itemprop="name"]')
         sb.assert_text("integrations")
