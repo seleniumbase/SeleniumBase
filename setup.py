23a07841f0feae8dffac1b4c2bcf60eddc4bc3f0
Michael Mintz <mdmintz@gmail.com>
2020-09-29 20:10:05 -0400

    Version 1.49.19

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ if sys.argv[-1] == 'publish':
 
 setup(
     name='seleniumbase',
-    version='1.49.18',
+    version='1.49.19',
     description='A complete framework for Web-UI testing | seleniumbase.io',
     long_description=long_description,
     long_description_content_type='text/markdown',
