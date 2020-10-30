fdec0a1fe1702ad1570776b37b4d41696775214f
Michael Mintz <mdmintz@gmail.com>
2020-10-30 17:59:00 -0400

    Update "wait_for_angularjs()" (skip alert-handling)

--- seleniumbase/fixtures/js_utils.py
+++ seleniumbase/fixtures/js_utils.py
@@ -48,13 +48,6 @@ def execute_async_script(driver, script, timeout=settings.EXTREME_TIMEOUT):
 
 
 def wait_for_angularjs(driver, timeout=settings.LARGE_TIMEOUT, **kwargs):
-    try:
-        # If there's an alert, skip
-        driver.switch_to.alert
-        return
-    except Exception:
-        # If there's no alert, continue
-        pass
     if not settings.WAIT_FOR_ANGULARJS:
         return
 
