14f110c3de19a2313e254a0b767d0caadbb3f460
albertteoh <albert.teoh@logz.io>
2021-02-21 21:18:12 +1100

    Run jaeger
    Signed-off-by: albertteoh <albert.teoh@logz.io>

--- examples/jaeger_ui_test.py
+++ examples/jaeger_ui_test.py
@@ -1,22 +1,43 @@
 from seleniumbase import BaseCase
+import docker
 
 
 class MyTestClass(BaseCase):
 
     def test_basics(self):
-        url = "https://store.xkcd.com/collections/posters"
+        client = docker.from_env()
+        container = client.containers.run(
+            image="jaegertracing/all-in-one:1.21",
+            detach="true",
+            name="jaeger",
+            environment=["COLLECTOR_ZIPKIN_HTTP_PORT=9411"],
+            ports={
+                '5775':'5775/udp',
+                '6831':'6831/udp',
+                '6832':'6832/udp',
+                '5778':'5778',
+                '16686':'16686',
+                '14268':'14268',
+                '14250':'14250',
+                '9411':'9411',
+            },
+            remove=True,
+        )
+        url = "http://localhost:16686/"
         self.open(url)
-        self.type('input[name="q"]', "xkcd book")
-        self.click('input[value="Search"]')
-        self.assert_text("xkcd: volume 0", "h3")
-        self.open("https://xkcd.com/353/")
-        self.assert_title("xkcd: Python")
-        self.assert_element('img[alt="Python"]')
-        self.click('a[rel="license"]')
-        self.assert_text("free to copy and reuse")
-        self.go_back()
-        self.click_link("About")
-        self.assert_exact_text("xkcd.com", "h2")
+        container.logs()
+        container.stop()
+        #self.type('input[name="q"]', "xkcd book")
+        #self.click('input[value="Search"]')
+        #self.assert_text("xkcd: volume 0", "h3")
+        #self.open("https://xkcd.com/353/")
+        #self.assert_title("xkcd: Python")
+        #self.assert_element('img[alt="Python"]')
+        #self.click('a[rel="license"]')
+        #self.assert_text("free to copy and reuse")
+        #self.go_back()
+        #self.click_link("About")
+        #self.assert_exact_text("xkcd.com", "h2")
 
         ####
 
