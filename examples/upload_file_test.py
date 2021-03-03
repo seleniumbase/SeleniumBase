a7a2eacba017c29f03269084f5de1d13e1afb88b
Michael Mintz <mdmintz@gmail.com>
2021-03-03 15:53:47 -0500

    Update example tests

--- examples/upload_file_test.py
+++ examples/upload_file_test.py
@@ -1,10 +1,10 @@
+""" Testing the self.choose_file() method. """
+
 import os
 from seleniumbase import BaseCase
 
 
 class FileUploadButtonTests(BaseCase):
-
-    """ The main purpose of this is to test the self.choose_file() method. """
 
     def test_file_upload_button(self):
         self.open("https://www.w3schools.com/jsref/tryit.asp"
@@ -15,7 +15,7 @@ class FileUploadButtonTests(BaseCase):
         self.add_css_style(zoom_in)
         self.highlight('input[type="file"]')
         dir_name = os.path.dirname(os.path.abspath(__file__))
-        file_path = dir_name + "/example_logs/screenshot.png"
+        file_path = os.path.join(dir_name, "example_logs/screenshot.png")
         self.choose_file('input[type="file"]', file_path)
         self.demo_mode = True  # Adds highlighting to the assert statement
         self.assert_element('input[type="file"]')
