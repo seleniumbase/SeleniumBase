dd573ab8a20ca0fb5ef01c03d8d4e6305f594c61
Michael Mintz <mdmintz@gmail.com>
2021-03-23 02:42:15 -0400

    Add the "test_shadow_dom" example test

---
+++ examples/test_shadow_dom.py
@@ -0,0 +1,64 @@
+""" Shadow DOM test.
+    First download files from PyPI.
+    Then search for them on a multi-layered Shadow DOM page.
+    This uses the "::shadow" selector for piercing shadow-root elements.
+    Here's the URL that contains Shadow DOM: chrome://downloads/ """
+
+
+from seleniumbase import BaseCase
+
+
+class ShadowDomTests(BaseCase):
+
+    def download_tar_file_from_pypi(self, package):
+        self.open("https://pypi.org/project/%s/#files" % package)
+        pkg_header = self.get_text("h1.package-header__name").strip()
+        pkg_name = pkg_header.replace(" ", "-")
+        tar_file = pkg_name + ".tar.gz"
+        tar_selector = 'div#files a[href$="%s"]' % tar_file
+        self.click(tar_selector)
+        return tar_file
+
+    def test_shadow_dom(self):
+        if self.browser != "chrome":
+            print("\n  This test is for Google Chrome only!")
+            self.skip('This test is for Google Chrome only!')
+        if self.headless:
+            print("\n  This test does not run in headless mode!")
+            self.skip('This test does not run in headless mode!')
+
+        file_name_1 = self.download_tar_file_from_pypi("sbase")
+        file_name_2 = self.download_tar_file_from_pypi("tensorpy")
+
+        self.open("chrome://downloads/")
+        search_icon = (
+            "downloads-manager::shadow downloads-toolbar::shadow"
+            " cr-toolbar::shadow cr-toolbar-search-field::shadow"
+            " cr-icon-button")
+        search_input = (
+            "downloads-manager::shadow downloads-toolbar::shadow"
+            " cr-toolbar::shadow cr-toolbar-search-field::shadow"
+            " #searchInput")
+        clear_search_icon = (
+            "downloads-manager::shadow downloads-toolbar::shadow"
+            " cr-toolbar::shadow cr-toolbar-search-field::shadow"
+            " #clearSearch")
+        file_link = (
+            "downloads-manager::shadow #downloadsList"
+            " downloads-item::shadow #file-link")
+        remove_button = (
+            "downloads-manager::shadow #downloadsList"
+            " downloads-item::shadow #remove")
+        no_downloads_area = "downloads-manager::shadow #no-downloads"
+
+        self.assert_element(search_icon)
+        self.type(search_input, "sbase")
+        self.assert_text(file_name_1, file_link)
+        print("\n  Download 1: %s" % self.get_text(file_link))
+        self.type(search_input, "tensorpy")
+        self.assert_text(file_name_2, file_link)
+        print("  Download 2: %s" % self.get_text(file_link))
+        self.click(clear_search_icon)
+        self.click(remove_button)
+        self.click(remove_button)
+        self.assert_text("Files you download appear here", no_downloads_area)
