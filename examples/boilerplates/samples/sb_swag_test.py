20403f267b6b3d0282059f2a3aee6eefb606729d
Michael Mintz <mdmintz@gmail.com>
2021-03-23 02:45:52 -0400

    Add a test with the Page Object Model and the "sb" fixture

---
+++ examples/boilerplates/samples/sb_swag_test.py
@@ -0,0 +1,18 @@
+""" Classic Page Object Model with the "sb" fixture """
+
+
+class LoginPage():
+
+    def login_to_swag_labs(self, sb, username):
+        sb.open("https://www.saucedemo.com/")
+        sb.type("#user-name", username)
+        sb.type("#password", "secret_sauce")
+        sb.click('input[type="submit"]')
+
+
+class MyTests():
+
+    def test_swag_labs_login(self, sb):
+        LoginPage().login_to_swag_labs(sb, "standard_user")
+        sb.assert_element("#inventory_container")
+        sb.assert_text("Products", "div.product_label")
