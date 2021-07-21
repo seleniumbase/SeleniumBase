26ec0652b5bc3ec2e410ce675485c2561ef6d78c
Michael Mintz <mdmintz@gmail.com>
2021-07-21 13:23:53 -0400

    Update example tests

--- examples/parameterized_test.py
+++ examples/parameterized_test.py
@@ -7,7 +7,7 @@ class GoogleTests(BaseCase):
         [
             ["PyPI", "pypi.org", 'img[alt="PyPI"]'],
             ["Wikipedia", "www.wikipedia.org", "img.central-featured-logo"],
-            ["SeleniumBase", "seleniumbase/SeleniumBase", 'img[title*="Sel"]'],
+            ["SeleniumBase GitHub", "base/SeleniumBase", 'img[title*="Sele"]'],
         ]
     )
     def test_parameterized_google_search(self, search_key, expected_text, img):
