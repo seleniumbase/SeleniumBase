d6638579cd400255da6f382b36bbfd5f96543f39
Michael Mintz <mdmintz@gmail.com>
2021-09-27 15:06:14 -0400

    Generate better selectors in "--recorder" mode

--- seleniumbase/js_code/recorder_js.py
+++ seleniumbase/js_code/recorder_js.py
@@ -36,6 +36,10 @@ var cssPathByAttribute = function(el, attr) {
         if (el.hasAttribute(attr) &&
             el.getAttribute(attr).length > 0) {
             the_attr = el.getAttribute(attr);
+            if (the_attr.includes('"'))
+                the_attr = the_attr.replace('"', '\\"');
+            if (the_attr.includes("'"))
+                the_attr = the_attr.replace("'", "\\'");
             selector += '[' + attr + '="' + the_attr + '"]';
             path.unshift(selector);
             break;
@@ -56,9 +60,8 @@ var cssPathByAttribute = function(el, attr) {
 var ssOccurrences = function(string, subString, allowOverlapping) {
     string += '';
     subString += '';
-    if (subString.length <= 0) {
+    if (subString.length <= 0)
         return (string.length + 1);
-    }
     var n = 0;
     var pos = 0;
     var step = allowOverlapping ? 1 : subString.length;
@@ -142,33 +145,54 @@ var getBestSelector = function(el) {
     contains_tags.push('a');
     contains_tags.push('b');
     contains_tags.push('i');
-    contains_tags.push('td');
     contains_tags.push('h1');
     contains_tags.push('h2');
     contains_tags.push('h3');
     contains_tags.push('h4');
+    contains_tags.push('td');
     contains_tags.push('code');
     contains_tags.push('button');
+    contains_tags.push('strong');
     all_by_tag = [];
+    inner_text = el.innerText.trim();
     for (var i = 0; i < contains_tags.length; i++) {
         if (tag_name == contains_tags[i] &&
-            el.innerText.trim().length > 1 &&
-            el.innerText.trim().length <= 64)
+            inner_text.length >= 2 && inner_text.length <= 64)
         {
             t_count = 0;
-            inner_text = el.innerText.trim();
             all_by_tag[i] = document.querySelectorAll(contains_tags[i]);
             for (var j = 0; j < all_by_tag[i].length; j++) {
                 if (all_by_tag[i][j].innerText.includes(inner_text))
-                {
                     t_count += 1;
-                }
             }
-            if (t_count === 1 && !inner_text.includes('\n'))
-            {
+            if (t_count === 1 && !inner_text.includes('\n')) {
                 inner_text = inner_text.replace("'", "\\'");
                 inner_text = inner_text.replace('"', '\\"');
                 return tag_name += ':contains("'+inner_text+'")';
+            }
+        }
+    }
+    if (tag_name == "span" && inner_text.length > 1 && inner_text.length <= 64)
+    {
+        parent_element = el.parentElement;
+        parent_tag_name = parent_element.tagName.toLowerCase();
+        grand_element = parent_element.parentElement;
+        grand_tag_name = grand_element.tagName.toLowerCase();
+        if (parent_tag_name == "button" || grand_tag_name == "button") {
+            qsa_element = "span";
+            if (parent_tag_name == "button")
+                qsa_element = "button > span";
+            else { qsa_element = "button > "+parent_tag_name+" > span" }
+            t_count = 0;
+            all_el_found = document.querySelectorAll(qsa_element);
+            for (var j = 0; j < all_el_found.length; j++) {
+                if (all_el_found[j].innerText.includes(inner_text))
+                    t_count += 1;
+            }
+            if (t_count === 1 && !inner_text.includes('\n')) {
+                inner_text = inner_text.replace("'", "\\'");
+                inner_text = inner_text.replace('"', '\\"');
+                return qsa_element += ':contains("'+inner_text+'")';
             }
         }
     }
@@ -187,19 +211,17 @@ var getBestSelector = function(el) {
 
 var AllTheAnchorTags = document.getElementsByTagName("a");
 for (var i = 0; i < AllTheAnchorTags.length; i++) {
-    AllTheAnchorTags[i].addEventListener('click',
-        function (event) {
-            if (this.origin &&
-                this.origin != 'null' &&
-                this.origin != document.location.origin)
-            {
-                if (this.hasAttribute('href'))
-                {
-                    event.preventDefault();
-                    window.open(this.href, '_blank').focus();
-                }
+    AllTheAnchorTags[i].addEventListener('click', function (event) {
+        if (this.origin &&
+            this.origin != 'null' &&
+            this.origin != document.location.origin)
+        {
+            if (this.hasAttribute('href')) {
+                event.preventDefault();
+                window.open(this.href, '_blank').focus();
             }
-        },
+        }
+    },
     false);
 }
 
@@ -207,21 +229,18 @@ var reset_recorder_state = function() {
     document.recorded_actions = [];
     sessionStorage.setItem('pause_recorder', 'no');
     const d_now = Date.now();
-    if (sessionStorage.getItem('recorder_activated') === 'yes')
-    {
+    if (sessionStorage.getItem('recorder_activated') === 'yes') {
         ss_ra = JSON.parse(sessionStorage.getItem('recorded_actions'));
         document.recorded_actions = ss_ra;
         w_orig = window.location.origin;
         w_href = window.location.href;
         ra_len = document.recorded_actions.length;
-        if (ra_len > 0 &&
-            document.recorded_actions[ra_len-1][0] === 'begin')
-        {
+        if (ra_len > 0 && document.recorded_actions[ra_len-1][0] === 'begin') {
             document.recorded_actions.pop();
             document.recorded_actions.push(['begin', w_orig, w_href, d_now]);
         }
         else if (ra_len > 0 &&
-            document.recorded_actions[ra_len-1][0] === '_url_')
+                 document.recorded_actions[ra_len-1][0] === '_url_')
         {
             document.recorded_actions.pop();
             document.recorded_actions.push(['_url_', w_orig, w_href, d_now]);
@@ -230,9 +249,8 @@ var reset_recorder_state = function() {
             document.recorded_actions.push(['_url_', w_orig, w_href, d_now]);
         }
     }
-    else
-    {
-        sessionStorage.setItem('recorder_activated', 'yes')
+    else {
+        sessionStorage.setItem('recorder_activated', 'yes');
         w_orig = window.location.origin;
         w_href = window.location.href;
         document.recorded_actions.push(['begin', w_orig, w_href, d_now]);
@@ -280,9 +298,8 @@ document.body.addEventListener('dragstart', function (event) {
     {
         document.recorded_actions.pop();
     }
-    if (element.draggable === true) {
+    if (element.draggable === true)
         document.recorded_actions.push(['drags', selector, '', d_now]);
-    }
     json_rec_act = JSON.stringify(document.recorded_actions);
     sessionStorage.setItem('recorded_actions', json_rec_act);
 });
@@ -292,8 +309,7 @@ document.body.addEventListener('dragend', function (event) {
     if (sessionStorage.getItem('pause_recorder') === 'yes')
         return;
     ra_len = document.recorded_actions.length;
-    if (ra_len > 0 &&
-        document.recorded_actions[ra_len-1][0] === 'drags')
+    if (ra_len > 0 && document.recorded_actions[ra_len-1][0] === 'drags')
     {
         document.recorded_actions.pop();
         json_rec_act = JSON.stringify(document.recorded_actions);
@@ -309,8 +325,7 @@ document.body.addEventListener('drop', function (event) {
     const element = event.target;
     const selector = getBestSelector(element);
     ra_len = document.recorded_actions.length;
-    if (ra_len > 0 &&
-        document.recorded_actions[ra_len-1][0] === 'drags')
+    if (ra_len > 0 && document.recorded_actions[ra_len-1][0] === 'drags')
     {
         drg_s = document.recorded_actions[ra_len-1][1];
         document.recorded_actions.pop();
@@ -337,15 +352,13 @@ document.body.addEventListener('change', function (event) {
     }
     else if (tag_name === 'input' && element.type === 'range')
     {
-        if (ra_len > 0 &&
-            document.recorded_actions[ra_len-1][1] === selector)
+        if (ra_len > 0 && document.recorded_actions[ra_len-1][1] === selector)
         {
             document.recorded_actions.pop();
             ra_len = document.recorded_actions.length;
         }
         // Do it twice for click and multiple changes.
-        if (ra_len > 0 &&
-            document.recorded_actions[ra_len-1][1] === selector)
+        if (ra_len > 0 && document.recorded_actions[ra_len-1][1] === selector)
         {
             document.recorded_actions.pop();
             ra_len = document.recorded_actions.length;
@@ -353,10 +366,8 @@ document.body.addEventListener('change', function (event) {
         value = element.value;
         document.recorded_actions.push(['set_v', selector, value, d_now]);
     }
-    else if (tag_name === 'input' && element.type === 'file')
-    {
-        if (ra_len > 0 &&
-            document.recorded_actions[ra_len-1][1] === selector)
+    else if (tag_name === 'input' && element.type === 'file') {
+        if (ra_len > 0 && document.recorded_actions[ra_len-1][1] === selector)
         {
             document.recorded_actions.pop();
             ra_len = document.recorded_actions.length;
@@ -368,19 +379,14 @@ document.body.addEventListener('change', function (event) {
         document.recorded_actions[ra_len-1][1] === selector &&
         tag_name === 'input' && element.type === 'checkbox')
     {
-        // The checkbox state only needs to be set once.
+        // The checkbox state only needs to be set once. (Pop duplicates.)
         document.recorded_actions.pop();
         ra_len = document.recorded_actions.length;
-        if (ra_len > 0 &&
-            document.recorded_actions[ra_len-1][1] === selector)
-        {
-            // Pop the duplicate if present.
+        if (ra_len > 0 && document.recorded_actions[ra_len-1][1] === selector)
             document.recorded_actions.pop();
-        }
     }
     // Go back to `if`, not `else if`.
-    if (tag_name === 'input' && element.type === 'checkbox' &&
-        element.checked)
+    if (tag_name === 'input' && element.type === 'checkbox' && element.checked)
     {
         document.recorded_actions.push(['c_box', selector, 'yes', d_now]);
     }
@@ -402,14 +408,11 @@ document.body.addEventListener('mousedown', function (event) {
     const selector = getBestSelector(element);
     ra_len = document.recorded_actions.length;
     tag_name = element.tagName.toLowerCase();
-    if (ra_len > 0 &&
-        document.recorded_actions[ra_len-1][0] === 'mo_dn')
-    {
+    if (ra_len > 0 && document.recorded_actions[ra_len-1][0] === 'mo_dn') {
         document.recorded_actions.pop();
     }
-    if (tag_name === 'select')
-    {
-        // Do Nothing (Handle select in 'change' action.)
+    if (tag_name === 'select') {
+        // Do Nothing. (Handle select in 'change' action.)
     }
     else {
         document.recorded_actions.push(['mo_dn', selector, '', d_now]);
@@ -427,53 +430,76 @@ document.body.addEventListener('mouseup', function (event) {
     const selector = getBestSelector(element);
     ra_len = document.recorded_actions.length;
     tag_name = element.tagName.toLowerCase();
+    parent_element = element.parentElement;
+    parent_tag_name = parent_element.tagName.toLowerCase();
+    grand_element = "";
+    grand_tag_name = "";
+    origin = "";
+    if (parent_element.parentElement != 'null') {
+        grand_element = parent_element.parentElement;
+        grand_tag_name = grand_element.tagName.toLowerCase();
+    }
     if (ra_len > 0 &&
         document.recorded_actions[ra_len-1][1] === selector &&
         (document.recorded_actions[ra_len-1][0] === 'mo_dn' ||
-         tag_name === 'a') && tag_name !== 'select')
+         tag_name === 'a' || parent_tag_name === 'a') &&
+        tag_name !== 'select')
     {
-        ahref = '';
+        href = '';
         if (tag_name === 'a' &&
             element.hasAttribute('href') &&
             element.getAttribute('href').length > 0 &&
             element.origin != 'null')
         {
-            // Because getAttribute('href') could start with '/'.
-            ahref = element.href;
+            href = element.href;
+            origin = element.origin;
+        }
+        else if (parent_tag_name === 'a' &&
+            parent_element.hasAttribute('href') &&
+            parent_element.getAttribute('href').length > 0 &&
+            parent_element.origin != 'null')
+        {
+            href = parent_element.href;
+            origin = parent_element.origin;
+        }
+        else if (grand_tag_name === 'a' &&
+            grand_element.hasAttribute('href') &&
+            grand_element.getAttribute('href').length > 0 &&
+            grand_element.origin != 'null')
+        {
+            href = grand_element.href;
+            origin = grand_element.origin;
         }
         document.recorded_actions.pop();
         child_sep = ' > ';
-        if (tag_name === "a" && !element.hasAttribute('onclick') &&
-            selector.includes(child_sep) && ahref.length > 0)
+        child_count = ssOccurrences(selector, child_sep);
+        if ((tag_name === "a" && !element.hasAttribute('onclick') &&
+             child_count > 0 && href.length > 0) ||
+            (parent_tag_name === "a" && href.length > 0 &&
+             child_count > 1 && !parent_element.hasAttribute('onclick')) ||
+            (grand_tag_name === "a" && href.length > 0 &&
+             child_count > 2 && !grand_element.hasAttribute('onclick')))
         {
-            origin = element.origin;
             w_orig = window.location.origin;
-            if (origin === w_orig)
-            {
-                document.recorded_actions.push(
-                    ['_url_', origin, ahref, d_now]);
+            if (origin === w_orig) {
+                document.recorded_actions.push(['_url_', origin, href, d_now]);
             }
             else {
-                document.recorded_actions.push(
-                    ['begin', origin, ahref, d_now]);
+                document.recorded_actions.push(['begin', origin, href, d_now]);
             }
         }
         else {
-            document.recorded_actions.push(
-                ['click', selector, ahref, d_now]);
+            document.recorded_actions.push(['click', selector, href, d_now]);
         }
         // Switch to hover_click() if in a dropdown.
-        if (element.parentElement.classList.contains(
-                'dropdown-content') &&
-            element.parentElement.parentElement.classList.contains(
-                'dropdown'))
+        if (element.parentElement.classList.contains('dropdown-content') &&
+            element.parentElement.parentElement.classList.contains('dropdown'))
         {
             ch_s = selector;
             pa_el = element.parentElement.parentElement;
             pa_s = getBestSelector(pa_el);
             if (pa_el.childElementCount >= 2 &&
-                !pa_el.firstElementChild.classList.contains(
-                'dropdown-content'))
+               !pa_el.firstElementChild.classList.contains('dropdown-content'))
             {
                 pa_el = pa_el.firstElementChild;
                 pa_s = getBestSelector(pa_el);
@@ -532,7 +558,7 @@ document.body.addEventListener('keydown', function (event) {
 });
 document.body.addEventListener('keyup', function (event) {
     if (typeof document.recorded_actions === 'undefined')
-            reset_recorder_state();
+        reset_recorder_state();
     // Controls for Pausing and Resuming the Recorder.
     if (event.key.toLowerCase() === 'escape' &&
         sessionStorage.getItem('pause_recorder') === 'no')
@@ -582,8 +608,7 @@ document.body.addEventListener('keyup', function (event) {
             {
                 skip_input = true;
             }
-        if (!skip_input)
-        {
+        if (!skip_input) {
             document.recorded_actions.push(
                 ['input', selector, element.value, d_now]);
         }
