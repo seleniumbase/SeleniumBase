b69e11ac1f0d51663cff159b276d7e91f5b1a60c
Michael Mintz <mdmintz@gmail.com>
2021-11-17 19:36:08 -0500

    Refresh Python dependencies

--- setup.py
+++ setup.py
@@ -123,7 +123,7 @@ setup(
         'packaging>=21.2;python_version>="3.6"',
         'setuptools>=44.1.1;python_version<"3.5"',
         'setuptools>=50.3.2;python_version>="3.5" and python_version<"3.6"',
-        'setuptools>=59.0.1;python_version>="3.6"',
+        'setuptools>=59.1.1;python_version>="3.6"',
         'setuptools-scm>=5.0.2;python_version<"3.6"',
         'setuptools-scm>=6.3.2;python_version>="3.6"',
         'tomli>=1.2.2;python_version>="3.6"',
@@ -139,7 +139,7 @@ setup(
         'parso==0.7.1;python_version<"3.6"',
         'parso==0.8.2;python_version>="3.6"',
         'jedi==0.17.2;python_version<"3.6"',
-        'jedi==0.18.0;python_version>="3.6"',
+        'jedi==0.18.1;python_version>="3.6"',
         'idna==2.10;python_version<"3.6"',  # Must stay in sync with "requests"
         'idna==3.3;python_version>="3.6"',  # Must stay in sync with "requests"
         'chardet==3.0.4;python_version<"3.5"',  # Stay in sync with "requests"
@@ -163,7 +163,7 @@ setup(
         "cssselect==1.1.0",
         "sortedcontainers==2.4.0",
         'filelock==3.2.1;python_version<"3.6"',
-        'filelock==3.3.2;python_version>="3.6"',
+        'filelock==3.4.0;python_version>="3.6"',
         'fasteners==0.16;python_version<"3.5"',
         'fasteners==0.16.3;python_version>="3.5"',
         "execnet==1.9.0",
@@ -227,7 +227,7 @@ setup(
         'Pillow==7.2.0;python_version>="3.5" and python_version<"3.6"',
         'Pillow==8.4.0;python_version>="3.6"',
         'typing-extensions==3.10.0.2;python_version<"3.8"',  # Sync with "rich"
-        'rich==10.13.0;python_version>="3.6" and python_version<"4.0"',
+        'rich==10.14.0;python_version>="3.6" and python_version<"4.0"',
         'tornado==5.1.1;python_version<"3.5"',
         'tornado==6.1;python_version>="3.5"',
         'pdfminer.six==20191110;python_version<"3.5"',
