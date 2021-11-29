701973473ba9b8ef032d8a52f76b236aaca55f86
Michael Mintz <mdmintz@gmail.com>
2021-11-29 01:30:48 -0500

    Add compatibility with Selenoid Selenium grids

---
+++ examples/capabilities/selenoid_cap_file.py
@@ -0,0 +1,8 @@
+# Desired capabilities example file for Selenoid Grid
+#
+# The same result can be achieved on the command-line with:
+#     --cap-string='{"selenoid": "true"}'
+
+capabilities = {
+    "selenoid": "true",
+}
