5b096ec0b19f5c4905744985fad0228c59390f89
Michael Mintz <mdmintz@gmail.com>
2021-12-07 13:55:16 -0500

    Refresh Python dependencies

--- setup.py
+++ setup.py
@@ -123,7 +123,7 @@ setup(
         'packaging>=21.3;python_version>="3.6"',
         'setuptools>=44.1.1;python_version<"3.5"',
         'setuptools>=50.3.2;python_version>="3.5" and python_version<"3.6"',
-        'setuptools>=59.4.0;python_version>="3.6"',
+        'setuptools>=59.5.0;python_version>="3.6"',
         'setuptools-scm>=5.0.2;python_version<"3.6"',
         'setuptools-scm>=6.3.2;python_version>="3.6"',
         'tomli>=1.2.2;python_version>="3.6"',
@@ -144,7 +144,7 @@ setup(
         'idna==3.3;python_version>="3.6"',  # Must stay in sync with "requests"
         'chardet==3.0.4;python_version<"3.5"',  # Stay in sync with "requests"
         'chardet==4.0.0;python_version>="3.5"',  # Stay in sync with "requests"
-        'charset-normalizer==2.0.8;python_version>="3.5"',  # Sync "requests"
+        'charset-normalizer==2.0.9;python_version>="3.5"',  # Sync "requests"
         "urllib3==1.26.7",  # Must stay in sync with "requests"
         'requests==2.26.0;python_version<"3.5"',
         'requests==2.25.1;python_version>="3.5" and python_version<"3.6"',
