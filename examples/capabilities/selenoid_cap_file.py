435b989d75b9e57cf2fe5fec6892c481a278a102
Michael Mintz <mdmintz@gmail.com>
2022-02-09 18:56:08 -0500

    Update an example capabilities file

--- examples/capabilities/selenoid_cap_file.py
+++ examples/capabilities/selenoid_cap_file.py
@@ -1,10 +1,12 @@
 # Desired capabilities example file for Selenoid Grid
 #
-# The same result can be achieved on the command-line with:
+# The same result can be achieved on the command-line. Eg:
 #     --cap-string='{"selenoid:options": {"enableVNC": true}}'
 
 capabilities = {
-    "screenResolution": "1280x1024x24",
+    "acceptSslCerts": True,
+    "acceptInsecureCerts": True,
+    "screenResolution": "1920x1080x24",
     "selenoid:options": {
         "enableVNC": True,
         "enableVideo": False,
