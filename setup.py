d54b5b6ddeef1a6ae1bdfb2aa4a735bd5e1490b2
Michael Mintz <mdmintz@gmail.com>
2022-03-12 18:38:49 -0500

    Refresh Python dependencies

--- setup.py
+++ setup.py
@@ -241,7 +241,7 @@ setup(
         'Pillow==9.0.1;python_version>="3.7"',
         'typing-extensions==3.10.0.2;python_version<"3.6"',  # <3.8 for "rich"
         'typing-extensions==4.0.0;python_version>="3.6" and python_version<"3.8"',  # noqa: E501
-        'rich==11.2.0;python_version>="3.6" and python_version<"4.0"',
+        'rich==12.0.0;python_version>="3.6" and python_version<"4.0"',
         'tornado==5.1.1;python_version<"3.5"',
         'tornado==6.1;python_version>="3.5"',
         'pdfminer.six==20191110;python_version<"3.5"',
