650da9df7c4e54aae7f6db898991ad4c02afcc8e
Michael Mintz <mdmintz@gmail.com>
2022-03-31 18:22:21 -0400

    Update an example test

--- examples/xpath_test.py
+++ examples/xpath_test.py
@@ -7,4 +7,5 @@ class XPathTests(BaseCase):
         self.assert_element('//h1[(text()="Demo Page")]')
         self.type('//*[@id="myTextInput"]', "XPath Test!")
         self.click('//button[starts-with(text(),"Click Me")]')
+        self.assert_element('//button[contains(., "Purple")]')
         self.assert_text("SeleniumBase", '//table/tbody/tr[1]/td[2]/h2')
