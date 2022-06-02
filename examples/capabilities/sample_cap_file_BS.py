493c84a49c14e371bed3dece17dabad351b34121
Michael Mintz <mdmintz@gmail.com>
2022-06-02 14:34:35 -0400

    Update sample capabilities files

--- examples/capabilities/sample_cap_file_BS.py
+++ examples/capabilities/sample_cap_file_BS.py
@@ -1,11 +1,11 @@
 # Desired capabilities example file for BrowserStack
 # Generate from https://www.browserstack.com/automate/capabilities
 desired_cap = {
-    "os" : "Windows",
-    "os_version" : "11",
-    "browser" : "Chrome",
-    "browser_version" : "101.0",
-    "browserstack.local" : "false",
-    "browserstack.debug" : "true",
-    "browserstack.selenium_version" : "4.1.2",
+    "os": "Windows",
+    "os_version": "11",
+    "browser": "Chrome",
+    "browser_version": "101.0",
+    "browserstack.local": "false",
+    "browserstack.debug": "true",
+    "browserstack.selenium_version": "4.1.2",
 }
