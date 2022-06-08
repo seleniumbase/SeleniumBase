05de7c96985bb5f0dc7314afedec06be230c972d
Michael Mintz <mdmintz@gmail.com>
2022-06-08 00:19:12 -0400

    Update an example test

--- examples/test_login.py
+++ examples/test_login.py
@@ -3,14 +3,12 @@ from seleniumbase import BaseCase
 
 class SwagLabsLoginTests(BaseCase):
     def login_to_swag_labs(self):
-        """Login to Swag Labs and verify that login was successful."""
         self.open("https://www.saucedemo.com")
         self.type("#user-name", "standard_user")
         self.type("#password", "secret_sauce")
         self.click('input[type="submit"]')
 
     def test_swag_labs_login(self):
-        """This test checks standard login for the Swag Labs store."""
         self.login_to_swag_labs()
         self.assert_element("#inventory_container")
         self.assert_element('.inventory_item:contains("Sauce Labs Backpack")')
