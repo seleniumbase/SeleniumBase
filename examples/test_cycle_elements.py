7b1dfeab79040e03aa7a9e247028d5da4e37cf4f
Michael Mintz <mdmintz@gmail.com>
2022-07-03 15:48:46 -0400

    Add test that tabs through and clicks the active element

---
+++ examples/test_cycle_elements.py
@@ -0,0 +1,13 @@
+from seleniumbase import BaseCase
+
+
+class CycleTests(BaseCase):
+    def test_cycle_elements_with_tab_and_press_enter(self):
+        """ Test pressing the tab key to cycle through elements.
+            Then click on the active element and verify actions.
+            This can all be performed by using a single command.
+            The "\t" is the tab key. The "\n" is the RETURN key. """
+        self.open("seleniumbase.io/demo_page")
+        self.assert_text("This Text is Green", "#pText")
+        self.send_keys("html", "\t\t\t\t\n")
+        self.assert_text("This Text is Purple", "#pText")
