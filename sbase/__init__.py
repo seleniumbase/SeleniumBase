607bf0dbf1afb25d4dde3eee4d83c95c08b280ac
Michael Mintz <mdmintz@gmail.com>
2022-11-27 08:46:34 -0500

    Add "sbase" shortcuts for "seleniumbase"

--- sbase/__init__.py
+++ sbase/__init__.py
@@ -1,6 +1,8 @@
+from seleniumbase import __version__  # noqa
 from seleniumbase import BaseCase  # noqa
 from seleniumbase import decorators  # noqa
 from seleniumbase import Driver  # noqa
+from seleniumbase import DriverContext  # noqa
 from seleniumbase import encryption  # noqa
 from seleniumbase import get_driver  # noqa
 from seleniumbase import js_utils  # noqa
