4903822b6c26133774a0d682647a43dd756be8e8
Michael Mintz <mdmintz@gmail.com>
2022-12-02 02:02:35 -0500

    Update example tests

--- examples/test_mfa_login.py
+++ examples/test_mfa_login.py
@@ -10,7 +10,7 @@ class TestMFALogin(BaseCase):
         self.assert_text("Welcome!", "h1")
         self.highlight("img#image1")  # A fancier assert_element() call
         self.click('a:contains("This Page")')  # Use :contains() on any tag
-        self.save_screenshot_to_logs()  # In "./latest_logs/" folder.
+        self.save_screenshot_to_logs()  # ("./latest_logs" folder for test)
         self.click_link("Sign out")  # Link must be "a" tag. Not "button".
         self.assert_element('a:contains("Sign in")')
         self.assert_exact_text("You have been signed out!", "#top_message")
