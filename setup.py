d6fda06c316c6d30d9e1df8c445223f85ed57c20
Michael Mintz <mdmintz@gmail.com>
2022-12-23 23:01:22 -0500

    Refresh optional dependencies

--- setup.py
+++ setup.py
@@ -250,7 +250,7 @@ setup(
         "coverage": [
             'coverage==5.5;python_version<"3.6"',
             'coverage==6.2;python_version>="3.6" and python_version<"3.7"',
-            'coverage==7.0.0;python_version>="3.7"',
+            'coverage==7.0.1;python_version>="3.7"',
             'pytest-cov==2.12.1;python_version<"3.6"',
             'pytest-cov==4.0.0;python_version>="3.6"',
         ],
