d1b3fa65e347b298933e8ac9fa84daaf7f42a32c
Michael Mintz <mdmintz@gmail.com>
2023-01-19 00:05:54 -0500

    Add a test for displaying CDP events in UC Mode

---
+++ examples/uc_cdp_events.py
@@ -0,0 +1,21 @@
+from pprint import pformat
+from seleniumbase import BaseCase
+
+if __name__ == "__main__":
+    from pytest import main
+    main([__file__, "--uc", "--uc-cdp", "-s"])
+
+
+class CDPTests(BaseCase):
+    def test_display_cdp_events(self):
+        if not self.undetectable or not self.uc_cdp_events:
+            self.get_new_driver(undetectable=True, uc_cdp_events=True)
+        # (To print everything, use "*". Otherwise select specific headers.)
+        # self.driver.add_cdp_listener("*", lambda data: print(pformat(data)))
+        self.driver.add_cdp_listener(
+            "Network.requestWillBeSentExtraInfo",
+            lambda data: print(pformat(data))
+        )
+        self.open("https://nowsecure.nl/#relax")
+        self.assert_text("OH YEAH, you passed!", "h1", timeout=6.75)
+        self.sleep(2)
