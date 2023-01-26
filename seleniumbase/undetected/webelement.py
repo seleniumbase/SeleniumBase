86273f441a7a853a67798834c1834842ea55427b
Michael Mintz <mdmintz@gmail.com>
2023-01-25 21:04:33 -0500

    Make general improvements to UC Mode

--- seleniumbase/undetected/webelement.py
+++ seleniumbase/undetected/webelement.py
@@ -5,9 +5,7 @@ from seleniumbase.config import settings
 class WebElement(selenium.webdriver.remote.webelement.WebElement):
     def uc_click(self):
         super().click()
-        if hasattr(settings, "SKIP_JS_WAITS") and settings.SKIP_JS_WAITS:
-            pass
-        elif (
+        if (
             hasattr(settings, "PAGE_LOAD_STRATEGY")
             and settings.PAGE_LOAD_STRATEGY == "none"
         ):
