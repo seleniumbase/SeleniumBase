87be221825269c575734e6997de1da9afcfb4dff
Michael Mintz <mdmintz@gmail.com>
2023-05-09 10:28:50 -0400

    Update the user-agent test

--- examples/user_agent_test.py
+++ examples/user_agent_test.py
@@ -4,9 +4,11 @@ BaseCase.main(__name__, __file__)
 
 class UserAgentTests(BaseCase):
     def test_user_agent(self):
-        self.open("https://www.whatsmyua.info/")
-        self.highlight("#rawUa", loops=2)
-        user_agent_detected = self.get_text("#rawUa")
+        self.open("https://my-user-agent.com/")
+        zoom_in = "#ua_string{zoom: 1.8;-moz-transform: scale(1.8);}"
+        self.add_css_style(zoom_in)
+        self.highlight("#ua_string")
+        user_agent_detected = self.get_text("#ua_string")
         original_user_agent = user_agent_detected
         if not self.user_agent:
             # Using the built-in user-agent string
@@ -15,7 +17,7 @@ class UserAgentTests(BaseCase):
             # User-agent was overridden using: --agent=STRING
             print("\n\nUser-Agent override: %s" % user_agent_detected)
         if not (self.headless or self.headless2 or self.xvfb):
-            self.sleep(3)
+            self.sleep(2.75)
 
         # Now change the user-agent using "execute_cdp_cmd()"
         if not self.is_chromium():
@@ -33,12 +35,16 @@ class UserAgentTests(BaseCase):
                     "NF/6.0.1.15.4 NintendoBrowser/5.1.0.20393"
                 },
             )
-            self.open("https://www.whatsmyua.info/")
-            self.highlight("#rawUa", loops=2)
-            user_agent_detected = self.get_text("#rawUa")
+            self.open("about:blank")
+            self.sleep(0.05)  # Enough to see that the page was refreshed
+            self.open("https://my-user-agent.com/")
+            zoom_in = "#ua_string{zoom: 1.8;-moz-transform: scale(1.8);}"
+            self.add_css_style(zoom_in)
+            self.highlight("#ua_string")
+            user_agent_detected = self.get_text("#ua_string")
             print("\nUser-Agent override: %s" % user_agent_detected)
             if not (self.headless or self.headless2 or self.xvfb):
-                self.sleep(3)
+                self.sleep(2.75)
         finally:
             # Reset the user-agent back to the original
             self.execute_cdp_cmd(
