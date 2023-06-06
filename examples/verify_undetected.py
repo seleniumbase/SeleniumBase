d1a3b998a31cd67b627f6953f1bb357fba998b92
Michael Mintz <mdmintz@gmail.com>
2023-06-06 16:44:49 -0400

    Update example tests

--- examples/verify_undetected.py
+++ examples/verify_undetected.py
@@ -5,12 +5,12 @@ from seleniumbase import BaseCase
 
 if __name__ == "__main__":
     from pytest import main
-    main([__file__, "--uc", "--incognito", "-s", "--uc-cdp-events"])
+    main([__file__, "--uc", "--guest", "--uc-cdp", "-s"])
 
 
 class UndetectedTest(BaseCase):
     def verify_success(self):
-        self.assert_text("OH YEAH, you passed!", "h1", timeout=6.75)
+        self.assert_text("OH YEAH, you passed!", "h1", timeout=7.25)
         self.post_message("Selenium wasn't detected!", duration=2.8)
         self._print("\n Success! Website did not detect Selenium! ")
 
@@ -19,7 +19,6 @@ class UndetectedTest(BaseCase):
 
     def test_browser_is_undetected(self):
         self.open("https://nowsecure.nl/#relax")
-        self.hide_elements("iframe")
         try:
             self.verify_success()
         except Exception:
