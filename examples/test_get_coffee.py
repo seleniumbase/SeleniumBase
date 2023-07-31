c15d19c2f80f4e84c63952b1be9292a41eeb429c
Michael Mintz <mdmintz@gmail.com>
2023-07-31 02:33:13 -0400

    Add test_get_coffee.py

---
+++ examples/test_get_coffee.py
@@ -0,0 +1,20 @@
+"""Use SeleniumBase to get coffee"""
+from seleniumbase import BaseCase
+BaseCase.main(__name__, __file__)
+
+
+class GetCoffeeTest(BaseCase):
+    def test_get_coffee(self):
+        self.open("https://seleniumbase.io/coffee/")
+        self.assert_title("Coffee Cart")
+        self.assert_link_text("cart (0)")
+        self.assert_element('[data-sb="Mocha"]')
+        self.click('div[data-sb="Mocha"]')
+        self.assert_link_text("cart (1)")
+        self.click_link_text("cart (1)")
+        self.assert_exact_text("Total: $8.00", ".pay")
+        self.click("button.pay")
+        self.type("input#name", "Selenium Coffee")
+        self.type("input#email", "test@test.test")
+        self.click("button#submit-payment")
+        self.assert_text("Thanks", "#app div.success")
