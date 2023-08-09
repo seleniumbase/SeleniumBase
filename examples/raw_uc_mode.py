2ccb7b56daafc55971cf510473a1339fe3d9ff71
Michael Mintz <mdmintz@gmail.com>
2023-08-09 14:31:28 -0400

    Add a new UC Mode example

---
+++ examples/raw_uc_mode.py
@@ -0,0 +1,11 @@
+"""SB Manager using "uc"-mode for evading bot-detection."""
+from seleniumbase import SB
+
+with SB(uc=True) as sb:
+    sb.open("https://nowsecure.nl/#relax")
+    sb.sleep(2)
+    if not sb.is_text_visible("OH YEAH, you passed!", "h1"):
+        sb.get_new_driver(uc=True)
+        sb.open("https://nowsecure.nl/#relax")
+        sb.sleep(2)
+    sb.assert_text("OH YEAH, you passed!", "h1", timeout=3)
