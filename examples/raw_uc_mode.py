e8e1a81fccaf90b112f01922ad4147d14c596e4c
Michael Mintz <mdmintz@gmail.com>
2023-08-13 21:29:01 -0400

    Update an example test

--- examples/raw_uc_mode.py
+++ examples/raw_uc_mode.py
@@ -3,9 +3,9 @@ from seleniumbase import SB
 
 with SB(uc=True) as sb:
     sb.open("https://nowsecure.nl/#relax")
-    sb.sleep(2)
+    sb.sleep(3)
     if not sb.is_text_visible("OH YEAH, you passed!", "h1"):
-        sb.get_new_driver(uc=True)
+        sb.get_new_driver(undetectable=True)
         sb.open("https://nowsecure.nl/#relax")
-        sb.sleep(2)
+        sb.sleep(3)
     sb.assert_text("OH YEAH, you passed!", "h1", timeout=3)
