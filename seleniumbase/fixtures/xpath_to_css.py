751fc182ff09310b93d0e06d982172e09e095b53
Michael Mintz <mdmintz@gmail.com>
2023-10-19 12:30:21 -0400

    Update the XPath-to-CSS selector converter

--- seleniumbase/fixtures/xpath_to_css.py
+++ seleniumbase/fixtures/xpath_to_css.py
@@ -2,7 +2,7 @@
 import re
 
 _sub_regexes = {
-    "tag": r"([a-zA-Z][-a-zA-Z0-9]{0,31}|\*)",
+    "tag": r"([a-zA-Z][-a-zA-Z0-9]{0,40}|\*)",
     "attribute": r"[.a-zA-Z_:][-\w:.]*(\(\))?)",
     "value": r"\s*[\w/:][-/\w\s,:;.\S]*",
 }
