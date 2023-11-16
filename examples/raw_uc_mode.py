8b0524684506ddffb86b4f33f87387887f73dcf6
Michael Mintz <mdmintz@gmail.com>
2023-11-15 21:16:55 -0500

    Update example tests

--- examples/raw_uc_mode.py
+++ examples/raw_uc_mode.py
@@ -2,12 +2,14 @@
 from seleniumbase import SB
 
 with SB(uc=True) as sb:
-    sb.driver.get("https://nowsecure.nl/#relax")
-    sb.sleep(1)
+    sb.driver.uc_open_with_tab("https://nowsecure.nl/#relax")
+    sb.sleep(1.2)
     if not sb.is_text_visible("OH YEAH, you passed!", "h1"):
         sb.get_new_driver(undetectable=True)
-        sb.driver.get("https://nowsecure.nl/#relax")
-        sb.sleep(1)
+        sb.driver.uc_open_with_reconnect(
+            "https://nowsecure.nl/#relax", reconnect_time=3
+        )
+        sb.sleep(1.2)
     if not sb.is_text_visible("OH YEAH, you passed!", "h1"):
         if sb.is_element_visible('iframe[src*="challenge"]'):
             with sb.frame_switch('iframe[src*="challenge"]'):
