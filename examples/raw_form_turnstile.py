fc6e7d56bd246061629694658f68b4c5362f5635
Michael Mintz <mdmintz@gmail.com>
2024-01-28 20:36:40 -0500

    Update example tests

--- examples/raw_form_turnstile.py
+++ examples/raw_form_turnstile.py
@@ -3,7 +3,7 @@ from seleniumbase import SB
 
 def open_the_form_turnstile_page(sb):
     sb.driver.uc_open_with_reconnect(
-        "https://seleniumbase.io/apps/form_turnstile", reconnect_time=2.27,
+        "https://seleniumbase.io/apps/form_turnstile", reconnect_time=2.5,
     )
 
 
