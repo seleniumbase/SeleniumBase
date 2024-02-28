992d940ea00c89ad3deaa8cfaeec69071ce9be88
Michael Mintz <mdmintz@gmail.com>
2024-02-27 21:34:23 -0500

    Update the default reconnect_time for "uc_click()"

--- seleniumbase/undetected/webelement.py
+++ seleniumbase/undetected/webelement.py
@@ -14,7 +14,7 @@ class WebElement(selenium.webdriver.remote.webelement.WebElement):
         else:
             super().click()
         if not reconnect_time:
-            self._parent.reconnect(0.42)
+            self._parent.reconnect(0.45)
         else:
             self._parent.reconnect(reconnect_time)
 
