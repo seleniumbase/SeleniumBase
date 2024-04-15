475785aaa37b1edcad0df25c2628045a6704903d
Michael Mintz <mdmintz@gmail.com>
2024-04-15 12:38:08 -0400

    Update an example

--- examples/raw_pixelscan.py
+++ examples/raw_pixelscan.py
@@ -4,7 +4,8 @@ with SB(uc=True, incognito=True, test=True) as sb:
     sb.driver.uc_open_with_reconnect("https://pixelscan.net/", 10)
     sb.remove_elements("jdiv")  # Remove chat widgets
     sb.assert_text("No automation framework detected", "pxlscn-bot-detection")
-    sb.assert_text("You are not masking your fingerprint")
+    not_masking = "You are not masking your fingerprint"
+    sb.assert_text(not_masking, "pxlscn-fingerprint-masking")
     sb.highlight("span.text-success", loops=8)
     sb.sleep(1)
     sb.highlight("pxlscn-fingerprint-masking div", loops=9, scroll=False)
