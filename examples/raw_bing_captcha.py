149afd6cc223f50ff071d4903f7dc4be5952ea02
Michael Mintz <mdmintz@gmail.com>
2024-07-24 15:55:04 -0400

    Update examples

--- examples/raw_bing_captcha.py
+++ examples/raw_bing_captcha.py
@@ -1,16 +1,6 @@
 from seleniumbase import SB
 
-
 with SB(uc=True, test=True) as sb:
     url = "https://www.bing.com/turing/captcha/challenge"
-    sb.driver.uc_open_with_reconnect(url, 1.25)
-    sb.add_css_style("iframe{zoom: 2}")  # Make it bigger
-    sb.switch_to_frame("iframe")
-    if not sb.is_element_visible("div#success"):
-        sb.driver.uc_open_with_reconnect(url, 4.05)
-        sb.add_css_style("iframe{zoom: 2}")
-        sb.switch_to_frame("iframe")
-    sb.highlight("div#success", loops=2)
-    sb.assert_text("Success!", "span#success-text")
-    sb.activate_demo_mode()  # See asserts as they happen
-    sb.assert_element("svg#success-icon")
+    sb.uc_open_with_reconnect(url, 4)
+    sb.uc_gui_click_captcha()
--- examples/raw_nopecha.py
+++ examples/raw_nopecha.py
@@ -1,16 +1,7 @@
 from seleniumbase import SB
 
 with SB(uc=True, test=True) as sb:
-    sb.uc_open_with_disconnect("nopecha.com/demo/turnstile", 3.5)
-    sb.uc_gui_press_keys("\t\t ")
-    sb.sleep(3.5)
-    sb.connect()
-    sb.uc_gui_handle_cf("#example-container5 iframe")
-
-    if sb.is_element_visible("#example-container0 iframe"):
-        sb.switch_to_frame("#example-container0 iframe")
-        sb.assert_element("circle.success-circle")
-        sb.switch_to_parent_frame()
-
-    sb.set_messenger_theme(location="top_center")
-    sb.post_message("SeleniumBase wasn't detected!", duration=3)
+    sb.uc_open_with_reconnect("nopecha.com/demo/turnstile", 3.2)
+    sb.uc_gui_click_captcha("#example-container0")
+    sb.uc_gui_click_captcha("#example-container5")
+    sb.sleep(3)
