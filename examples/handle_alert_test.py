0d641fc3904c86304360b3ecc7a072eb5194f2bd
Michael Mintz <mdmintz@gmail.com>
2024-09-25 21:43:49 -0400

    Update an example

--- examples/handle_alert_test.py
+++ examples/handle_alert_test.py
@@ -7,6 +7,7 @@ class HandleAlertTests(BaseCase):
         self.open("about:blank")
         self.execute_script('window.alert("ALERT!!!");')
         self.sleep(1)  # Not needed (Lets you see the alert pop up)
+        self.assert_true(self.is_alert_present())
         self.accept_alert()
         self.sleep(1)  # Not needed (Lets you see the alert go away)
         self.execute_script('window.prompt("My Prompt","defaultText");')
@@ -15,6 +16,7 @@ class HandleAlertTests(BaseCase):
         self.assert_equal(alert.text, "My Prompt")  # Not input field
         self.dismiss_alert()
         self.sleep(1)  # Not needed (Lets you see the alert go away)
+        self.assert_false(self.is_alert_present())
         if self.browser == "safari" and self._reuse_session:
             # Alerts can freeze Safari if reusing the browser session
             self.driver.quit()
