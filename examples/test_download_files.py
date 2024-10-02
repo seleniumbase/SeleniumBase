2c4511187908d19fd973e77c90767f213e3d1e19
Michael Mintz <mdmintz@gmail.com>
2024-10-01 23:09:35 -0400

    Update an example

--- examples/test_download_files.py
+++ examples/test_download_files.py
@@ -52,9 +52,9 @@ class DownloadTests(BaseCase):
             self.download_file(whl_href)
             self.download_file(tar_href)
         else:
-            self.click(whl_selector)  # Download the "whl" file
+            self.js_click(whl_selector)  # Download the "whl" file
             self.sleep(0.1)
-            self.click(tar_selector)  # Download the "tar" file
+            self.js_click(tar_selector)  # Download the "tar" file
 
         # Verify that the downloaded files appear in the [Downloads Folder]
         # (This only guarantees that the exact file name is in the folder.)
