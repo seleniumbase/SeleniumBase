e0de30c6a21a103a3e97fb3fdc619a92262c4bc1
Michael Mintz <mdmintz@gmail.com>
2024-10-23 16:37:42 -0400

    Update examples

--- examples/raw_recaptcha.py
+++ examples/raw_recaptcha.py
@@ -2,7 +2,7 @@ from seleniumbase import SB
 
 with SB(uc=True, test=True) as sb:
     url = "https://seleniumbase.io/apps/recaptcha"
-    sb.uc_open_with_reconnect(url)
+    sb.activate_cdp_mode(url)
     sb.uc_gui_handle_captcha()  # Try with TAB + SPACEBAR
     sb.assert_element("img#captcha-success", timeout=3)
     sb.set_messenger_theme(location="top_left")
@@ -10,8 +10,8 @@ with SB(uc=True, test=True) as sb:
 
 with SB(uc=True, test=True) as sb:
     url = "https://seleniumbase.io/apps/recaptcha"
-    sb.uc_open_with_reconnect(url)
-    sb.uc_gui_click_captcha()  # Try with PyAutoGUI Click
+    sb.activate_cdp_mode(url)
+    sb.uc_gui_click_captcha('iframe[src*="/recaptcha/"]')
     sb.assert_element("img#captcha-success", timeout=3)
     sb.set_messenger_theme(location="top_left")
     sb.post_message("SeleniumBase wasn't detected", duration=3)
