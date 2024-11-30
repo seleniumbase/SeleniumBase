98379dd6416bd36ed8219ddcf6d7defe807bed80
Michael Mintz <mdmintz@gmail.com>
2024-11-29 22:45:36 -0500

    Add new CDP Mode examples

---
+++ examples/cdp_mode/raw_fingerprint.py
@@ -0,0 +1,12 @@
+from seleniumbase import SB
+
+with SB(uc=True, test=True, incognito=True) as sb:
+    url = "https://demo.fingerprint.com/playground"
+    sb.activate_cdp_mode(url)
+    sb.sleep(1)
+    sb.cdp.highlight('a[href*="browser-bot-detection"]')
+    bot_row_selector = 'table:contains("Bot") tr:nth-of-type(3)'
+    print(sb.cdp.get_text(bot_row_selector))
+    sb.cdp.assert_text("Bot Not detected", bot_row_selector)
+    sb.cdp.highlight(bot_row_selector)
+    sb.sleep(2)
