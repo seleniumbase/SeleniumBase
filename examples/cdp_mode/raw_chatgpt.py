633f4ec2a1471cb9d1ba436d1c924f377a4eabf8
Michael Mintz <mdmintz@gmail.com>
2025-01-29 11:51:51 -0500

    Update the ChatGPT test

--- examples/cdp_mode/raw_chatgpt.py
+++ examples/cdp_mode/raw_chatgpt.py
@@ -1,3 +1,4 @@
+from contextlib import suppress
 from seleniumbase import SB
 
 with SB(uc=True, test=True, ad_block=True) as sb:
@@ -6,8 +7,11 @@ with SB(uc=True, test=True, ad_block=True) as sb:
     query = "Compare Playwright to SeleniumBase in under 178 words"
     sb.type("#prompt-textarea", query)
     sb.click('button[data-testid="send-button"]')
-    print('Input for ChatGPT:\n"%s"' % query)
-    sb.sleep(12)
+    print('*** Input for ChatGPT: ***\n"%s"' % query)
+    with suppress(Exception):
+        # The "Send" button reappears when ChatGPT is done typing a response
+        sb.wait_for_element('button[data-testid="send-button"]', timeout=22)
     chat = sb.find_element('[data-message-author-role="assistant"] .markdown')
     soup = sb.get_beautiful_soup(chat.get_html()).get_text("\n").strip()
-    print("Response from ChatGPT:\n%s" % soup.replace("\n:", ":"))
+    print("*** Response from ChatGPT: ***\n%s" % soup.replace("\n:", ":"))
+    sb.sleep(3)
