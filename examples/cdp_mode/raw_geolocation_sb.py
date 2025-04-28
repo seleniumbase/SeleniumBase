614a7ab0d17526b7b05dcc0bd1feed9948e1592f
Michael Mintz <mdmintz@gmail.com>
2025-04-28 18:46:49 -0400

    Add a CDP Mode Geolocation example

---
+++ examples/cdp_mode/raw_geolocation_sb.py
@@ -0,0 +1,9 @@
+"""Geolocation example with CDP Mode"""
+from seleniumbase import SB
+
+with SB(uc=True, test=True) as sb:
+    url = "https://www.openstreetmap.org/"
+    sb.activate_cdp_mode(url, geoloc=(31.774390, 35.222450))
+    sb.click("span.geolocate")
+    sb.assert_url_contains("31.774390/35.222450")
+    sb.sleep(5)
