3cf1eaf9b0834496621e0f08488a3fbd350bda80
Michael Mintz <mdmintz@gmail.com>
2025-05-02 18:29:28 -0400

    Update examples

--- examples/cdp_mode/raw_cdp_nike.py
+++ examples/cdp_mode/raw_cdp_nike.py
@@ -4,7 +4,7 @@ url = "https://www.nike.com/"
 sb = sb_cdp.Chrome(url)
 sb.click('div[data-testid="user-tools-container"]')
 sb.sleep(1)
-search = "Road Racing Shoes"
+search = "Pegasus"
 sb.press_keys('input[type="search"]', search)
 sb.sleep(4)
 elements = sb.select_all('ul[data-testid*="products"] figure .details')
