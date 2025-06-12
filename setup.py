248b0ec11771b19943c06f35d0b2f027211b0365
Michael Mintz <mdmintz@gmail.com>
2025-06-12 14:08:09 -0400

    Update classifiers

--- setup.py
+++ setup.py
@@ -126,6 +126,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Topic :: Internet",
         "Topic :: Internet :: WWW/HTTP :: Browsers",
         "Topic :: Scientific/Engineering",
