725c6eab865d0ad5a9ffb1868c24cdfb409ba7bc
Michael Mintz <mdmintz@gmail.com>
2025-09-20 12:42:40 -0400

    Remove an old example

--- examples/raw_nopecha.py
+++
@@ -1,8 +0,0 @@
-from seleniumbase import SB
-
-with SB(uc=True, test=True) as sb:
-    sb.uc_open_with_reconnect("nopecha.com/demo/turnstile", 3.2)
-    if sb.is_element_visible("#example-container0"):
-        sb.uc_gui_click_captcha("#example-container0")
-    sb.uc_gui_click_captcha("#example-container5")
-    sb.sleep(3)
