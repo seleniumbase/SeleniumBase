07230e3963ec71fafbb443897bf9212db142b3d3
Michael Mintz <mdmintz@gmail.com>
2025-09-20 13:29:20 -0400

    Update CDP Mode examples

--- examples/cdp_mode/raw_cf_clearance.py
+++ examples/cdp_mode/raw_cf_clearance.py
@@ -1,16 +1,20 @@
 from seleniumbase import sb_cdp
 
+
+def get_cf_clearance_cookie(sb):
+    all_cookies = sb.get_all_cookies()
+    for cookie in all_cookies:
+        if cookie.name == "cf_clearance":
+            return cookie
+    return None
+
+
 url = "https://gitlab.com/users/sign_in"
-sb = sb_cdp.Chrome(url)
-sb.sleep(2.2)
-sb.gui_click_captcha()
-sb.sleep(2)
-cf_cookie = None
-all_cookies = sb.get_all_cookies()
-for cookie in all_cookies:
-    if cookie.name == 'cf_clearance':
-        cf_cookie = cookie
-        break
+sb = sb_cdp.Chrome(url, incognito=True)
+sb.sleep(2.2)  # Wait for CAPTCHA to load
+sb.gui_click_captcha()  # (Only if found)
+sb.sleep(2.2)  # Wait for CAPTCHA success
+cf_cookie = get_cf_clearance_cookie(sb)
 if cf_cookie:
     print("cf_clearance cookie: %s" % cf_cookie.value)
 else:
