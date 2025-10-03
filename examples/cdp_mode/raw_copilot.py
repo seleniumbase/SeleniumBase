3cc26bda42719d742b85c50d485e88b6b6eb61af
Michael Mintz <mdmintz@gmail.com>
2025-10-03 13:43:12 -0400

    Update an example

--- examples/cdp_mode/raw_copilot.py
+++ examples/cdp_mode/raw_copilot.py
@@ -12,7 +12,7 @@ with SB(uc=True, test=True, guest=True) as sb:
     sb.sleep(1.1)
     sb.click('button[title="Think Deeper"]')
     sb.sleep(1.1)
-    query = "How to migrate from Playwright to SeleniumBase?"
+    query = "How to start automating with SeleniumBase?"
     sb.press_keys(textarea, query)
     sb.sleep(1.1)
     sb.click('button[data-testid="submit-button"]')
@@ -20,14 +20,14 @@ with SB(uc=True, test=True, guest=True) as sb:
     sb.uc_gui_click_captcha()
     sb.sleep(2.5)
     sb.uc_gui_click_captcha()
-    sb.sleep(2.5)
+    sb.sleep(3.5)
     stop_button = '[data-testid="stop-button"]'
     thumbs_up = 'button[data-testid*="-thumbs-up-"]'
-    sb.wait_for_element_absent(stop_button, timeout=30)
-    sb.wait_for_element(thumbs_up, timeout=30)
-    sb.sleep(0.5)
+    sb.wait_for_element_absent(stop_button, timeout=40)
+    sb.wait_for_element(thumbs_up, timeout=20)
+    sb.sleep(0.6)
     sb.click('button[data-testid*="scroll-to-bottom"]')
-    sb.sleep(1.5)
+    sb.sleep(2.2)
     folder = "downloaded_files"
     file_name = "copilot_results.html"
     sb.save_page_source(file_name, folder)
