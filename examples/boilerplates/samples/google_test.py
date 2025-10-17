8c0e92d63420905db386b4c8e712c6feab5a1980
Michael Mintz <mdmintz@gmail.com>
2025-10-17 11:02:36 -0400

    Update examples

--- examples/boilerplates/samples/google_test.py
+++ examples/boilerplates/samples/google_test.py
@@ -9,19 +9,16 @@ except Exception:
 
 class GoogleTests(BaseCase):
     def test_google_dot_com(self):
-        if self.headless and self._multithreaded:
+        if self.headless:
             self.open_if_not_url("about:blank")
-            print("\n  Skipping test in headless multi-threaded mode.")
-            self.skip("Skipping test in headless multi-threaded mode.")
+            print("\n  Skipping test in headless mode.")
+            self.skip("Skipping test in headless mode.")
+        if not self.undetectable:
+            self.get_new_driver(undetectable=True)
         self.open("https://google.com/ncr")
         self.assert_title_contains("Google")
         self.sleep(0.05)
         self.save_screenshot_to_logs()  # ("./latest_logs" folder)
-        if not self.is_element_visible("iframe"):
-            self.sleep(1.5)  # A slow pop-up might appear
-        self.hide_elements('iframe')  # Hide "Sign in" pop-up
-        self.sleep(0.05)
-        self.save_screenshot_to_logs()
         self.type(HomePage.search_box, "github.com")
         self.assert_element(HomePage.search_button)
         self.assert_element(HomePage.feeling_lucky_button)
--- examples/raw_mobile.py
+++ examples/raw_mobile.py
@@ -4,9 +4,8 @@ driver = Driver(mobile=True)
 try:
     driver.open("https://www.roblox.com/")
     driver.assert_element("#download-the-app-container")
-    driver.assert_text("Roblox for Android", "p.roblox-for-platform")
-    driver.assert_text("Continue in App", "a.primary-link")
-    driver.highlight("p.roblox-for-platform", loops=8)
-    driver.highlight("a.primary-link", loops=8)
+    driver.assert_text("Roblox for Android")
+    driver.highlight('span:contains("Roblox for Android")', loops=8)
+    driver.highlight('span:contains("Continue in App")', loops=8)
 finally:
     driver.quit()
