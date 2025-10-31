7cfa79a2f80f00667e00d03b3282a93f1399ee10
Michael Mintz <mdmintz@gmail.com>
2025-10-31 13:35:17 -0400

    Update an example

--- examples/cdp_mode/raw_tiktok.py
+++ examples/cdp_mode/raw_tiktok.py
@@ -1,10 +1,10 @@
 from seleniumbase import SB
 
-with SB(uc=True, test=True, ad_block=True) as sb:
+with SB(uc=True, test=True, guest=True) as sb:
     url = "https://www.tiktok.com/@startrek?lang=en"
     sb.activate_cdp_mode(url)
-    sb.sleep(2)
+    sb.sleep(3)
     print(sb.get_text('h2[data-e2e="user-bio"]'))
     for i in range(54):
-        sb.cdp.scroll_down(12)
+        sb.scroll_down(12)
     sb.sleep(1)
