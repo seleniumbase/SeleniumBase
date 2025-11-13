bb412d02557478e728035e0b203e7e761b214a42
Michael Mintz <mdmintz@gmail.com>
2025-11-13 01:27:26 -0500

    Update CDP Mode examples

--- examples/cdp_mode/raw_planetmc.py
+++ examples/cdp_mode/raw_planetmc.py
@@ -1,6 +1,6 @@
 from seleniumbase import SB
 
-with SB(uc=True, test=True) as sb:
+with SB(uc=True, test=True, guest=True) as sb:
     url = "www.planetminecraft.com/account/sign_in/"
     sb.activate_cdp_mode(url)
     sb.sleep(1.2)
