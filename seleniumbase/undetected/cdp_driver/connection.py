b65b7250acc259a06b0bd19d4e293b567e2692d5
Michael Mintz <mdmintz@gmail.com>
2025-12-13 13:10:47 -0500

    Try to prevent a CDP Mode memory leak

--- seleniumbase/undetected/cdp_driver/connection.py
+++ seleniumbase/undetected/cdp_driver/connection.py
@@ -610,11 +610,11 @@ class Listener:
                 # Probably an event
                 try:
                     event = cdp.util.parse_json_event(message)
-                    event_tx = EventTransaction(event)
-                    if not self.connection.mapper:
-                        self.connection.__count__ = itertools.count(0)
-                    event_tx.id = next(self.connection.__count__)
-                    self.connection.mapper[event_tx.id] = event_tx
+                    # event_tx = EventTransaction(event)
+                    # if not self.connection.mapper:
+                    #     self.connection.__count__ = itertools.count(0)
+                    # event_tx.id = next(self.connection.__count__)
+                    # self.connection.mapper[event_tx.id] = event_tx
                 except Exception as e:
                     logger.info(
                         "%s: %s during parsing of json from event : %s"
@@ -639,12 +639,12 @@ class Listener:
                                 or inspect.iscoroutine(callback)
                             ):
                                 try:
-                                    await callback(event, self.connection)
+                                    asyncio.create_task(callback(event, self))
                                 except TypeError:
-                                    await callback(event)
+                                    asyncio.create_task(callback(event))
                             else:
                                 try:
-                                    callback(event, self.connection)
+                                    callback(event, self)
                                 except TypeError:
                                     callback(event)
                         except Exception as e:
