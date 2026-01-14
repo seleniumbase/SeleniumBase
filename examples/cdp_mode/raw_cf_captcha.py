c4a79967a078843ba58395c407c8517ca72e7bd7
Michael Mintz <mdmintz@gmail.com>
2026-01-14 01:31:10 -0500

    Update CDP Mode examples

--- examples/cdp_mode/raw_cf_captcha.py
+++ examples/cdp_mode/raw_cf_captcha.py
@@ -3,6 +3,7 @@ from seleniumbase import SB
 with SB(uc=True, test=True, guest=True) as sb:
     url = "https://www.cloudflare.com/login"
     sb.activate_cdp_mode(url)
-    sb.sleep(3)
+    sb.wait_for_element('div[data-testid*="challenge-widget"]')
+    sb.sleep(1.5)
     sb.solve_captcha()
     sb.sleep(3)
