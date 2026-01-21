b2277147f0260dc4091085b9014a8115541c2c48
Michael Mintz <mdmintz@gmail.com>
2026-01-21 15:02:43 -0500

    Update the TikTok example

--- examples/cdp_mode/raw_tiktok.py
+++ examples/cdp_mode/raw_tiktok.py
@@ -3,8 +3,14 @@ from seleniumbase import SB
 with SB(uc=True, test=True, guest=True) as sb:
     url = "https://www.tiktok.com/@startrek?lang=en"
     sb.activate_cdp_mode(url)
-    sb.sleep(3)
-    print(sb.get_text('h2[data-e2e="user-bio"]'))
-    for i in range(54):
-        sb.scroll_down(12)
+    sb.sleep(2.5)
+    sb.click_if_visible('button[data-close-button="true"]')
+    print("*** " + sb.get_text('h2[data-e2e="user-bio"]'))
+    for i in range(33):
+        sb.scroll_by_y(33)
+        sb.sleep(0.03)
+    items = sb.find_elements("picture img")
+    for i, item in enumerate(items):
+        print("* %s: %s" % (i, str(item.get_attribute("alt"))))
+    print("*** %s total items found!" % len(items))
     sb.sleep(1)
