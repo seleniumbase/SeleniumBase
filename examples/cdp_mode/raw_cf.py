392b74379b3a31024d446884bdf6ae9ac5686960
Michael Mintz <mdmintz@gmail.com>
2026-03-12 20:09:02 -0400

    Update CDP Mode examples

--- examples/cdp_mode/raw_cf.py
+++ examples/cdp_mode/raw_cf.py
@@ -1,16 +1,23 @@
-"""Using CDP Mode with PyAutoGUI to bypass CAPTCHAs."""
+"""Using CDP Mode to bypass CAPTCHAs in different ways."""
 from seleniumbase import SB
 
 with SB(uc=True, test=True, guest=True) as sb:
     url = "https://www.cloudflare.com/login"
     sb.activate_cdp_mode(url)
     sb.sleep(3)
-    sb.uc_gui_handle_captcha()  # PyAutoGUI press Tab and Spacebar
+    sb.uc_gui_handle_captcha()  # PyAutoGUI Tabs + Spacebar
     sb.sleep(3)
 
 with SB(uc=True, test=True, guest=True) as sb:
     url = "https://www.cloudflare.com/login"
     sb.activate_cdp_mode(url)
     sb.sleep(4)
-    sb.uc_gui_click_captcha()  # PyAutoGUI click. (Linux needs it)
+    sb.uc_gui_click_captcha()  # PyAutoGUI mouse click
+    sb.sleep(3)
+
+with SB(uc=True, test=True, guest=True) as sb:
+    url = "https://www.cloudflare.com/login"
+    sb.activate_cdp_mode(url)
+    sb.sleep(4)
+    sb.solve_captcha()  # CDP Input.dispatchMouseEvent
     sb.sleep(3)
