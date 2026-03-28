993b64640f582f417aec1b143fc8d59be7c3bd75
Michael Mintz <mdmintz@gmail.com>
2026-03-28 13:28:51 -0400

    Update a Stealthy Playwright example

--- examples/cdp_mode/playwright/raw_cf_cap_sync.py
+++ examples/cdp_mode/playwright/raw_cf_cap_sync.py
@@ -8,6 +8,6 @@ with sync_playwright() as p:
     browser = p.chromium.connect_over_cdp(endpoint_url)
     page = browser.contexts[0].pages[0]
     page.goto("https://www.cloudflare.com/login")
-    page.wait_for_timeout(4500)
+    page.wait_for_timeout(5000)
     sb.solve_captcha()
     page.wait_for_timeout(3000)
