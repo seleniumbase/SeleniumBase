95f1fcc513c8b778fe725ac97eb8794a91a5e3b7
Michael Mintz <mdmintz@gmail.com>
2026-04-04 18:03:52 -0400

    Update Pixelscan examples

--- examples/cdp_mode/raw_cdp_pixelscan.py
+++ examples/cdp_mode/raw_cdp_pixelscan.py
@@ -2,14 +2,13 @@ from seleniumbase import sb_cdp
 
 url = "https://pixelscan.net/fingerprint-check"
 sb = sb_cdp.Chrome(url, incognito=True)
-sb.remove_element("#headerBanner")
 sb.wait_for_element("pxlscn-dynamic-ad")
 sb.sleep(0.5)
 sb.remove_elements("pxlscn-dynamic-ad")
 sb.sleep(2)
 sb.assert_text("No masking detected", "pxlscn-fingerprint-masking")
 sb.assert_text("No automated behavior", "pxlscn-bot-detection")
-sb.highlight('span:contains("is consistent")')
+sb.highlight('span.status-success')
 sb.sleep(1)
 sb.highlight("pxlscn-fingerprint-masking p")
 sb.sleep(1)
--- examples/raw_pixelscan.py
+++ examples/raw_pixelscan.py
@@ -3,14 +3,13 @@ from seleniumbase import SB
 with SB(uc=True, test=True, incognito=True) as sb:
     url = "https://pixelscan.net/fingerprint-check"
     sb.activate_cdp_mode(url)
-    sb.remove_element("#headerBanner")
     sb.wait_for_element("pxlscn-dynamic-ad")
     sb.sleep(0.5)
     sb.remove_elements("pxlscn-dynamic-ad")
     sb.sleep(2)
     sb.assert_text("No masking detected", "pxlscn-fingerprint-masking")
     sb.assert_text("No automated behavior", "pxlscn-bot-detection")
-    sb.cdp.highlight('span:contains("is consistent")')
+    sb.cdp.highlight('span.status-success')
     sb.sleep(1)
     sb.cdp.highlight("pxlscn-fingerprint-masking p")
     sb.sleep(1)
