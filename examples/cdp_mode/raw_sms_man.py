319a3913856c62fb9dfcc493e32fe08b4fa4573f
Michael Mintz <mdmintz@gmail.com>
2026-04-20 21:13:24 -0400

    Rename and update an example

--- examples/cdp_mode/raw_sms_man.py
+++ examples/cdp_mode/raw_sms_man.py
@@ -1,10 +1,9 @@
-# An example of bypassing 2 consecutive CF CAPTCHAs"""
 from seleniumbase import SB
 
 with SB(uc=True, test=True) as sb:
     url = "https://sms-man.com/login"
     sb.activate_cdp_mode(url)
-    sb.sleep(2.2)
+    sb.sleep(3)
     if not sb.is_element_present('input[name="email"]'):
         sb.solve_captcha()
         sb.sleep(1)
