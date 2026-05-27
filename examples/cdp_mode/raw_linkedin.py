349ea8db8b86f82cffea8cdd52cf306d6efc0d20
Michael Mintz <mdmintz@gmail.com>
2026-05-26 22:13:02 -0400

    Speed up an example

--- examples/cdp_mode/raw_linkedin.py
+++ examples/cdp_mode/raw_linkedin.py
@@ -3,11 +3,11 @@ from seleniumbase import SB
 with SB(uc=True, test=True, ad_block=True) as sb:
     url = "https://www.linkedin.com/company/selenium"
     sb.activate_cdp_mode(url)
-    sb.sleep(2)
-    sb.click_if_visible('button[aria-label="Dismiss"]')
     sb.sleep(1)
-    for i in range(42):
-        sb.scroll_down(16)
+    sb.click_if_visible('button[aria-label="Dismiss"]')
+    sb.sleep(0.5)
+    for i in range(14):
+        sb.scroll_down(48)
     print(sb.get_page_title())
     sb.save_as_pdf_to_logs()
     sb.save_page_source_to_logs()
