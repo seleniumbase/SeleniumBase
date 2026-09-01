26470cfaf7c75842579acf4cf01b5eab96d8e864
Michael Mintz <mdmintz@gmail.com>
2026-09-01 12:51:01 -0400

    Refresh Python dependencies

--- setup.py
+++ setup.py
@@ -177,7 +177,7 @@ setup(
         'fasteners>=0.20',
         'mycdp>=1.4.0',
         'pynose>=1.5.5',
-        'platformdirs>=4.11.5',
+        'platformdirs>=4.11.7',
         'typing-extensions>=4.16.0',
         'sbvirtualdisplay>=1.4.0',
         'MarkupSafe>=3.0.3',
